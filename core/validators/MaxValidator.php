34a11193b840db6e46ab7e6f97019c1d30f2d581
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-12 22:02:19 -0400

    Added validation neede for register form

---
+++ core/validators/MaxValidator.php
@@ -0,0 +1,8 @@
+<?php
+class MaxValidator extends CustomValidator {
+    public function runValidation() {
+        $value = $this->_model->{$this->field};
+        $pass = (strlen($value) <= $this->rule);
+        return $pass;
+    }
+}
