004039198bf79baf3715ff42d374e1bdb89c764d
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-14 13:43:56 -0400

    Added phpdoc

--- core/Application.php
+++ core/Application.php
@@ -1,11 +1,21 @@
 <?php
 namespace Core;
+
+/**
+ * The Application class supports basic functional needs of the application.
+ */
 class Application {
     public function __construct() {
         $this->_set_reporting();
         $this->_unregister_globals();
     }
 
+    /**
+     * Manages the displaying of error messages and other reporting for this 
+     * application.
+     *
+     * @return void
+     */
     private function _set_reporting() {
         if(DEBUG) {
             //error_reporting(E_ALL);
@@ -19,6 +29,12 @@ class Application {
         }
     }
 
+    /**
+     * Unregister global variables that are created by default in PHP prior 
+     * to version 8.
+     *
+     * @return void
+     */
     private function _unregister_globals() {
         if(ini_get('register_globals')) {
             $globalsAry = ['_SESSION', '_COOKIE', '_POST', '_GET' , '_REQUEST', '_SERVER', '_ENV', '_FILES'];
--- core/Controller.php
+++ core/Controller.php
@@ -2,12 +2,25 @@
 namespace Core;
 use Core\Application;
 
+/**
+ * This is the parent Controller class.  It describes functions that are 
+ * available to all classes that extends this Controller class.
+ */
 class Controller extends Application {
     protected $_action;
     protected $_controller;
     public $request;
     public $view;
 
+    /**
+     * Constructor for the parent Controller class.  This constructor gets 
+     * called when an instance of the child class is instantiated.
+     *
+     * @param string $controller The name of the controller obtained while 
+     * parsing the URL.
+     * @param string $action The name of the action specified in the path of 
+     * the URL.
+     */
     public function __construct($controller, $action) {
         parent::__construct();
 
@@ -17,14 +30,26 @@ class Controller extends Application {
         $this->view = new View();
     }
 
-    public function jsonResponse($resp){
+    /**
+     * Sample jsonResponse for supporting AJAX requests.
+     *
+     * @param string $res The JSON response.
+     * @return void
+     */
+    public function jsonResponse($res){
         header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
         http_response_code(200);
-        echo json_encode($resp);
+        echo json_encode($res);
         exit;
     }
     
+    /**
+     * Loads the model provided to this controller.
+     *
+     * @param string $model The name of the model that we will used.
+     * @return void
+     */
     protected function load_model($model) {
         $modelPath = 'App\Models\\' . $model;
         if(class_exists($modelPath)) {
