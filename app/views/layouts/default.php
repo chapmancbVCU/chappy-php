d76f052ddc567515289334a17959722bd16a5dbe
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-14 18:22:02 -0400

    Added footer to layour

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -10,7 +10,7 @@ use Core\Session;
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title><?=$this->siteTitle()?></title>
     <link rel="stylesheet" href="<?=PROOT?>css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="<?=PROOT?>css/custom.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="<?=PROOT?>css/styles.css" media="screen" title="no title" charset="utf-8">
     <script src="<?=PROOT?>js/jQuery-2.2.4.min.js"></script>
     <script src="<?=PROOT?>js/bootstrap.min.js"></script>
 
@@ -22,5 +22,6 @@ use Core\Session;
       <?= Session::displayMessage() ?>
       <?= $this->content('body'); ?>
     </div>
+    <?php include 'footer.php'; ?>
   </body>
 </html>
