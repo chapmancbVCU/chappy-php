f254c5abd06bc4b2c6b4502d6bf36d15162f0c10
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-15 18:40:08 -0400

    Added phpdoc and added support in read function to sort results by ascending or descending order

--- core/DB.php
+++ core/DB.php
@@ -2,12 +2,11 @@
 namespace Core;
 use \PDO;
 use \PDOException;
-
+use Core\Helper;
 /**
  * Parent class for database connections.
  */
 class DB {
-    
     private $_count = 0;
     private $_error = false;
     private static $_instance = null;
@@ -17,8 +16,8 @@ class DB {
     private $_result;
     
     /**
-     * This constructor creates a new instance of the PDO object.  If there 
-     * are any failures the application quits with an error message.
+     * This constructor creates a new PDO object as an instance variable.  If 
+     * there are any failures the application quits with an error message.
      */
     private function __construct() {
         try {
@@ -29,9 +28,9 @@ class DB {
     }
 
     /**
-     * 
+     * Getter function for the private _count variable.
      *
-     * @return int 
+     * @return int The number of results found in an SQL query.
      */
     public function count() {
         return $this->_count;
@@ -40,6 +39,9 @@ class DB {
     /**
      * Performs delete operation against SQL database.
      *
+     * Example setup:
+     * $contacts = $db->delete('contacts', 3);
+     * 
      * @param string $table The name of the table that contains the record 
      * we want to delete.
      * @param int $id The primary key for the record we want to remove from a 
@@ -64,6 +66,27 @@ class DB {
         return $this->_error;
     }
 
+    /**
+     * Performs find operation against the database.  The user can use 
+     * parameters such as conditions, bind, order, limit, and sort.
+     * 
+     * Example setup:
+     * $contacts = $db->find('users', [
+     *     'conditions' => ["email = ?"],
+     *     'bind' => ['chad.chapman@email.com'],
+     *     'order' => "username",
+     *     'limit' => 5,
+     *     'sort' => 'DESC'
+     * ]);
+     *
+     * @param string $table The name or the table we want to perform 
+     * our query against
+     * @param array $params The values for the query.  They are the fields of 
+     * the table in our database.  The default value is an empty array.
+     * @param boolean|string  $class A default value of false, it contains the 
+     * name of the class we will build based on the name of a model.
+     * @return void
+     */
     public function find($table, $params = [], $class = false) {
         if($this->_read($table, $params, $class)) {
             return $this->results();
@@ -71,17 +94,42 @@ class DB {
         return false;
     }
 
-    public function findFirst($table, $params=[], $class = false) {
+    /**
+     * Returns the first result performed by an SQL query.  It is a wrapper
+     * for the _read function for this purpose.
+     *
+     * @param @param string $table The name or the table we want to perform 
+     * our query against
+     * @@param array $params The values for the query.  They are the fields of 
+     * the table in our database.  The default value is an empty array.
+     * @param boolean|string  $class A default value of false, it contains the 
+     * name of the class we will build based on the name of a model.
+     * @return void
+     */
+    public function findFirst($table, $params = [], $class = false) {
         if($this->_read($table, $params, $class)) {
             return $this->first();
         }
         return false;
     }
 
+    /**
+     * Returns first result in the _result array.
+     *
+     * @return object The first object in a _result array.
+     */
     public function first() {
         return (!empty($this->_result)) ? $this->_result[0] : []; 
     }
 
+    /**
+     * Returns columns for a table.
+     *
+     * @param string $table The name of the table we want to retrieve
+     * the column names.
+     * @return array An array of objects where each one represents a column 
+     * from a database table.
+     */
     public function getColumns($table) {
         return $this->query("SHOW COLUMNS FROM {$table}")->results();
     }
@@ -93,10 +141,27 @@ class DB {
         return self::$_instance;
     }
 
+    /**
+     * Perform insert operations against the database.
+     * 
+     * Example setup:
+     * $fields = [
+     *   'fname' => 'John',
+     *   'lname' => 'Doe',
+     *   'email' => 'example@email.com'
+     * ];
+     * $contacts = $db->insert('contacts', $fields);
+     * 
+     * @param string $table The name of the table we want to perform the 
+     * insert operation.
+     * @param array $fields The field names and the respective values we will 
+     * use to populate a database record.  The default value is an empty array.
+     * @return bool Report for whether or not the operation was successful.
+     */
     public function insert($table, $fields = []) {
-        $fieldString = '';
-        $valueString = '';
-        $values = [];
+        $fieldString = '';      // Table field
+        $valueString = '';      // Populated with ?
+        $values = [];           // Values we will bind when we build our query
 
         foreach($fields as $field => $value) {
             $fieldString .= '`' . $field . '`,';
@@ -114,10 +179,25 @@ class DB {
         } else return false;
     }
 
+    /**
+     * The primary key ID of the last insert operation.
+     *
+     * @return int The primary key ID from the last insert operation.
+     */
     public function lastID() {
         return $this->_lastInsertID;
     }
 
+    /**
+     * Perform query functions.
+     *
+     * @param string $sql The database query we will submit to the database.
+     * @param array $params The values for the query.  They are the fields of 
+     * the table in our database.  The default value is an empty array.
+     * @param boolean|string  $class A default value of false, it contains the 
+     * name of the class we will build based on the name of a model.
+     * @return object The results of the database query.
+     */
     public function query($sql, $params = [], $class = false) {
         $this->_error = false;
         if($this->_query = $this->_pdo->prepare($sql)) {
@@ -144,20 +224,25 @@ class DB {
     }
 
     /**
-     * Supports SELECT operations that maybe ran against a SQL database.
+     * Supports SELECT operations that maybe ran against a SQL database.  It 
+     * supports the ability to order and limit the number of results returned 
+     * from a database query.  The user can use parameters such as conditions, 
+     * bind, order, limit, and sort.
      *
      * @param string $table The name of the table that contains the 
      * record(s) we want to find.
-     * @param array $params
-     * @param [type] $class
+     * @param array $params The values for the query.  They are the fields of 
+     * the table in our database.  The default value is an empty array.
+     * @param boolean|string  $class A default value of false, it contains the 
+     * name of the class we will build based on the name of a model.
      * @return void
      */
-    protected function _read($table, $params=[], $class) {
+    protected function _read($table, $params = [], $class) {
         $conditionString = '';
         $bind = [];
         $order = '';
         $limit = '';
-
+        $sort = '';
         // Conditions
         if(isset($params['conditions'])) {
             if(is_array($params['conditions'])) {
@@ -190,7 +275,12 @@ class DB {
             $limit = ' LIMIT ' . $params['limit'];
         }
 
-        $sql = "SELECT * FROM {$table}{$conditionString}{$order}{$limit}";
+        // Sort - Untested
+        if(array_key_exists('sort', $params)) {
+            $sort = ' '. $params['sort'];
+        }
+
+        $sql = "SELECT * FROM {$table}{$conditionString}{$order}{$sort}{$limit}";
         if($this->query($sql, $bind, $class)) {
             if(!count($this->_result)) {
                 return false;
@@ -200,6 +290,12 @@ class DB {
         return false;
     }
 
+    /**
+     * Returns value of query results.
+     *
+     * @return array An array of object that contain results of a database 
+     * query.
+     */
     public function results() {
         return $this->_result;
     }
@@ -207,6 +303,13 @@ class DB {
     /**
      * Performs update operation on a SQL database record.
      *
+     * Example setup:
+     * $fields = [
+     *   'fname' => 'John',
+     *   'email' => 'example@email.com'
+     * ];
+     * $contacts = $db->update('contacts', 3, $fields);
+     * 
      * @param string $table $table The name of the table that contains the 
      * record we want to update.
      * @param int $id The primary key for the record we want to remove from a 
@@ -217,8 +320,8 @@ class DB {
      * we return false.
      */
     public function update($table, $id, $fields = []) {
-        $fieldString = '';
-        $values = [];
+        $fieldString = '';      // Table field
+        $values = [];           // Values we will bind when we build our query
 
         foreach($fields as $field => $value) {
             $fieldString .= ' ' . $field . ' = ?,';
