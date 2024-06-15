6bd23a546d113fbdd0de388494304b892e8ec696
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-14 21:45:59 -0400

    Added phpdoc comments

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,17 +2,30 @@
 namespace App\Controllers;
 use Core\Controller;
 
+/**
+ * Implements support for our Home controller.  Functions found in the class 
+ * will support tasks related to the home page.
+ */
 class HomeController extends Controller {
     public function __construct($controller, $action) {
         parent::__construct($controller, $action);
     }
 
-    /** Optional array of params can be passed into an action function as an 
-     *  array. */
+    /** 
+     * The default action for this controller.  It performs rendering of this 
+     * site's home page.
+     * 
+     * @return void
+     */
     public function indexAction() {
         $this->view->render('home/index');
     }
 
+    /**
+     * Demonstration for an Ajax request.
+     *
+     * @return void
+     */
     public function testAjaxAction(){
         $resp = ['success'=>true,'data'=>['id'=>23,'name'=>'Curtis','favorite_food'=>'bread']];
         $this->jsonResponse($resp);
--- core/View.php
+++ core/View.php
@@ -1,41 +1,46 @@
 <?php
 namespace Core;
-
+use Core\Helper;
+/**
+ * Handles operations related to views and its content
+ */
 class View {
-    protected $_head;
     protected $_body;
-    protected $_siteTitle = SITE_TITLE;
-    protected $_outputBuffer;
+    protected $_head;
     protected $_layout = DEFAULT_LAYOUT;
-
+    protected $_outputBuffer;
+    protected $_siteTitle = SITE_TITLE;
+    
+    /**
+     * Default constructor.
+     */
     public function __construct()
     {
         
     }
 
-    public function render($viewName) {
-        $viewAry = explode('/', $viewName);
-        $viewString = implode(DS, $viewAry);
-        if(file_exists(ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php')) {
-            include(ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php');
-            include(ROOT . DS . 'app' . DS . 'views' . DS . 'layouts' . DS . $this->_layout . '.php');
-        } else {
-            die('The view \"' . $viewName . '\" does not exist');
-        }
-    }
-
-
+    /**
+     * The content of the page.  The two types are head and body.  If 
+     * necessary, we can implement additional types of content.
+     *
+     * @param string $type The type of content we want to render.
+     * @return bool|mixed The type of content we want to render.  If it is not 
+     * a known type of content we return false;
+     */
     public function content($type) {
         if($type == 'head') return $this->_head;
         elseif($type == 'body') return $this->_body;
         else return false;
     }
 
-    public function start($type) {
-        $this->_outputBuffer = $type;
-        ob_start();
-    }
-
+    /**
+     * Sets the end for a particular section of content.  When called it 
+     * takes _outputBuffer, cleans it, and outputs it to the screen.  In the 
+     * absence of a previous call to the start() function a message requesting 
+     * you to call start() is displayed.
+     *
+     * @return void
+     */
     public function end() {
         if($this->_outputBuffer == 'head') {
             $this->_head = ob_get_clean();
@@ -46,23 +51,91 @@ class View {
         }
     }
 
-    public function siteTitle() {
-        return $this->_siteTitle;
-    }
-
-    public function setSiteTitle($title) {
-        $this->_siteTitle = $title;
-    }
-
-    public function setLayout($path) {
-        $this->_layout = $path;
-    }
-
+    /**
+     * Inserts a partial into another partial
+     *
+     * @param string $path Path to view.  Example: register/register
+     * @return void
+     */
     public function insert($path) {
         include ROOT . DS . 'app' . DS . 'views' . DS . $path . '.php';
     }
 
+    /**
+     * Performs render operations for a particular view.
+     * Example input: home/index
+     * 
+     * @param string $viewName The name of the view we want to render.
+     * @return void
+     */
+    public function render($viewName) {
+        // Separate into array to get view name.
+        $viewArray = explode('/', $viewName);
+        $viewString = implode(DS, $viewArray);
+
+        // Include view and layouts
+        if(file_exists(ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php')) {
+            include(ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php');
+            include(ROOT . DS . 'app' . DS . 'views' . DS . 'layouts' . DS . $this->_layout . '.php');
+        } else {
+            die('The view \"' . $viewName . '\" does not exist');
+        }
+    }
+
+    /**
+     * Includes a partial for our view.  Partial views assist with code 
+     * reuse.  An application of this would be forms.  The parameters are used 
+     * to build the path for the partial used in this function's include 
+     * statement.
+     *
+     * @param string $group The name of the parent view.
+     * @param string $partial The name of the partial view.
+     * @return void
+     */
     public function partial($group, $partial) {
         include ROOT . DS . 'app' . DS . 'views' . DS . $group . DS . 'partials' . DS . $partial . '.php';
     }
+
+    /**
+     * Sets the layout for the view.
+     *
+     * @param string $path  
+     * @return void
+     */
+    public function setLayout($path) {
+        $this->_layout = $path;
+    }
+
+    /**
+     * Setter function for site title of current page.
+     *
+     * @param string $title The site title for a particular page.
+     * @return void
+     */
+    public function setSiteTitle($title) {
+        $this->_siteTitle = $title;
+    }
+
+    /**
+     * Getter function for current page's site title.
+     *
+     * @return string The site title for a particular page.
+     */
+    public function siteTitle() {
+        return $this->_siteTitle;
+    }
+
+    /**
+     * When called this function establishes the beginning for a section 
+     * of content.  Anything between calls of this function and end() will be 
+     * included in our view.
+     *
+     * @param string $type The name of the type of content we want to include 
+     * in our view.
+     * @return void
+     */
+    public function start($type) {
+        $this->_outputBuffer = $type;
+        ob_start();
+    } 
 }
