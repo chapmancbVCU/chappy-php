2f466f36e6b0460c5092614953a8696a47b1642a
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-19 19:24:11 -0400

    Styling for titles

--- app/views/documentation/index.php
+++ app/views/documentation/index.php
@@ -1,7 +1,10 @@
 <?php $this->setSiteTitle("Custom MVC Framework Docs"); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center red">Custom MVC Framework Docs</h1>
+<h1 class="text-center">Custom MVC Framework Docs</h1>
 
+<div class="row align-items-center justify-content-center my-3">
+    <p class="text-center w-75">The MVC documentation contains descriptions for built in classes, functions, and JavaScript</p>
+</div>
 <ul>
     <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
     <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
--- app/views/documentation/java_script_docs.php
+++ app/views/documentation/java_script_docs.php
@@ -1,6 +1,6 @@
 <?php $this->setSiteTitle("Custom MVC Framework Docs"); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center red">JavaScript</h1>
+<h1 class="text-center">JavaScript</h1>
 
 <a href="<?=PROOT?>documentation" class="text-primary">Custom MVC Docs Home</a>
 <?php $this->end(); ?>
