f011789a385a2b7401dd06026f5f8b6e37129faa
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-19 19:43:00 -0400

    Added styling and template content

--- app/views/documentation/index.php
+++ app/views/documentation/index.php
@@ -5,11 +5,14 @@
 <div class="row align-items-center justify-content-center my-3">
     <p class="text-center w-75">The MVC documentation contains descriptions for built in classes, functions, and JavaScript</p>
 </div>
-<ul>
-    <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
-    <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
-    <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
-    <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
-</ul>
+<div class="ml-5">
+    <ul>
+        <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
+        <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
+        <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
+        <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
+    </ul>
+</div>
+
 
 <?php $this->end(); ?>
--- app/views/documentation/java_script_docs.php
+++ app/views/documentation/java_script_docs.php
@@ -2,5 +2,18 @@
 <?php $this->start('body'); ?>
 <h1 class="text-center">JavaScript</h1>
 
-<a href="<?=PROOT?>documentation" class="text-primary">Custom MVC Docs Home</a>
+<a class="ml-5" href="<?=PROOT?>documentation" class="text-primary">Custom MVC Docs Home</a>
+
+<div class="row align-items-center justify-content-center my-3">
+    <p class="text-center w-75">Template </p>
+</div>
+
+<div class="ml-5">
+    <ul>
+        <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
+        <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
+        <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
+        <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
+    </ul>
+</div>
 <?php $this->end(); ?>
