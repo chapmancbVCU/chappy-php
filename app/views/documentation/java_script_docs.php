cb521eec3a8a0536c3f4a8281dedae4c59de239e
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-22 13:37:17 -0400

    Applied layout to other doc pages

--- app/views/documentation/java_script_docs.php
+++ app/views/documentation/java_script_docs.php
@@ -1,19 +1,23 @@
 <?php $this->setSiteTitle("Custom MVC Framework Docs"); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center">JavaScript</h1>
+<?php include('docs_nav.php'); ?>
 
-<a class="ml-5" href="<?=PROOT?>documentation" class="text-primary">Custom MVC Docs Home</a>
+<div class="main">
+    <h1 class="text-center">JavaScript</h1>
 
-<div class="row align-items-center justify-content-center my-3">
-    <p class="text-center w-75">Template </p>
+    <a class="ml-5" href="<?=PROOT?>documentation" class="text-primary">Custom MVC Docs Home</a>
+
+    <div class="row align-items-center justify-content-center my-3">
+        <p class="text-center w-75">Template </p>
+    </div>
+    <div class="ml-5">
+        <ul>
+            <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
+            <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
+            <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
+            <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
+        </ul>
+    </div>
 </div>
-
-<div class="ml-5">
-    <ul>
-        <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
-        <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
-        <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
-        <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
-    </ul>
-</div>
+<script src="<?=PROOT?>public/js/docNavDropdown.js"></script>
 <?php $this->end(); ?>
