e4ee426d9652d076745085b5e421a62db5ad1d0f
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-22 19:54:13 -0400

    Initial commit

---
+++ app/views/documentation/register_controller_docs.php
@@ -0,0 +1,14 @@
+<?php $this->setSiteTitle("Contracts Controller - Custom MVC Framework Docs"); ?>
+<?php $this->start('body'); ?>
+<?php include('docs_nav.php'); ?>
+
+<div class="main">
+    <a href="<?=PROOT?>documentation/controllers" class="btn btn-xs btn-secondary">Controllers</a>
+    <h1 class="text-center">Contacts Controller Class</h1>
+    <div class="row align-items-center justify-content-center my-3">
+        <p class="text-center w-75">Implements support for our Contact Controller.  It contains actions for handling user interactions that will result in CRUD operations against the database.</p>
+    </div>
+
+    </div>
+    <script src="<?=PROOT?>public/js/docNavDropdown.js"></script>
+<?php $this->end(); ?>
