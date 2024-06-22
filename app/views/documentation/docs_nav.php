18dc279030befdcbbc00729c82b0c4699155e4ae
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-22 13:19:29 -0400

    First initial test for docs layout

---
+++ app/views/documentation/docs_nav.php
@@ -0,0 +1,16 @@
+<div class="sidenav bg-dark">
+    <button class="dropdown-btn">Dropdown1
+        <i class="float-right pr-1 dropdown-toggle"></i>
+    </button>
+    <div class="dropdown-container">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+    </div>
+    <button class="dropdown-btn">Dropdown2
+        <i class="float-right pr-1 dropdown-toggle"></i>
+    </button>
+    <div class="dropdown-container">
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+    </div>
+</div>
--- app/views/documentation/index.php
+++ app/views/documentation/index.php
@@ -1,18 +1,21 @@
 <?php $this->setSiteTitle("Custom MVC Framework Docs"); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center">Custom MVC Framework Docs</h1>
+<?php include('docs_nav.php'); ?>
 
-<div class="row align-items-center justify-content-center my-3">
-    <p class="text-center w-75">The MVC documentation contains descriptions for built in classes, functions, and JavaScript</p>
+<div class="main">
+    <h1 class="text-center">Custom MVC Framework Docs</h1>
+
+    <div class="row align-items-center justify-content-center my-3">
+        <p class="text-center w-75">The MVC documentation contains descriptions for built in classes, functions, and JavaScript</p>
+    </div>
+    <div class="ml-5">
+        <ul>
+            <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
+            <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
+            <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
+            <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
+        </ul>
+    </div>
 </div>
-<div class="ml-5">
-    <ul>
-        <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
-        <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
-        <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
-        <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
-    </ul>
-</div>
-
-
+<script src="<?=PROOT?>public/js/docNavDropdown.js"></script>
 <?php $this->end(); ?>
