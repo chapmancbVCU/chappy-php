47f05cb454d7fedf008a8f70b3d1e1bd5e36de0e
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-22 15:55:56 -0400

    Begin to add content to controller docs view

--- app/views/documentation/controllers_docs.php
+++ app/views/documentation/controllers_docs.php
@@ -5,12 +5,17 @@
 <div class="main">
     <h1 class="text-center">Controller Classes</h1>
     <div class="row align-items-center justify-content-center my-3">
-        <p class="text-center w-75">Description</p>
+        <p class="text-center w-75">
+            Here you will find information about the various stock controllers 
+            that are provided by this Model View Controller (MVC) framework.
+        </p>
     </div>
     <div class="ml-5">
         <ul>
-            <li><a href="<?=PROOT?>documentation/controllers" class="text-primary">Controllers</a></li>
-            <li><a href="<?=PROOT?>documentation/core" class="text-primary">Core</a></li>
+            <li><a href="<?=PROOT?>documentation/contactsController" class="text-primary">Contacts Controller: </a>
+                Handles views and Create, Read, Update, and Delete operations for the built in example contact management system.</li>
+            <li><a href="<?=PROOT?>documentation/homeController" class="text-primary">Home Controller: </a>
+                Supports actions and view for home controller.</li>
             <li><a href="<?=PROOT?>documentation/javaScript" class="text-primary">javaScript</a></li>
             <li><a href="<?=PROOT?>documentation/models" class="text-primary">Models</a></li>
         </ul>
