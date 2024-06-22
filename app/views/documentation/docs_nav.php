988d0c958d043a97a59eb1f77ecf22239e6b1104
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-22 14:56:59 -0400

    Added link to parent page of each section

--- app/views/documentation/docs_nav.php
+++ app/views/documentation/docs_nav.php
@@ -3,29 +3,25 @@
         <i class="float-right pr-1 dropdown-toggle"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
+        <a href="<?=PROOT?>documentation/controllers">Overview</a>
     </div>
     <button class="dropdown-btn">Core
         <i class="float-right pr-1 dropdown-toggle"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Link 3</a>
-        <a href="#">Link 4</a>
+        <a href="<?=PROOT?>documentation/core">Overview</a>
     </div>
     <button class="dropdown-btn">JavaScript
         <i class="float-right pr-1 dropdown-toggle"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Link 3</a>
-        <a href="#">Link 4</a>
+        <a href="<?=PROOT?>documentation/javaScript">Overview</a>
     </div>
     <button class="dropdown-btn">Models
         <i class="float-right pr-1 dropdown-toggle"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Link 3</a>
-        <a href="#">Link 4</a>
+        <a href="<?=PROOT?>documentation/models">Overview</a>
     </div>
     <a href="http://php.net/manual/en">PHP Docs<a>
 </div>
