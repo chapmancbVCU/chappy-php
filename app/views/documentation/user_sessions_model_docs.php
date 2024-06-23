04dc37df69d3fbdd8c919d9b7a8f46f6e096f103
Chad Chapman <chad.chapman2010@gmail.com>
2024-06-23 11:10:41 -0400

    Initial commit

---
+++ app/views/documentation/user_sessions_model_docs.php
@@ -0,0 +1,15 @@
+<?php $this->setSiteTitle("UserSessions Model - Custom MVC Framework Docs"); ?>
+<?php $this->start('body'); ?>
+<?php include('docs_nav.php'); ?>
+
+<div class="main">
+<a href="<?=PROOT?>documentation/models" class="btn btn-xs btn-secondary">Models</a>
+    <h1 class="text-center">UsersSessions Model Class</h1>
+    <div class="row align-items-center justify-content-center my-3">
+        <p class="text-center w-75">Implements support for our Contact Controller.  It contains actions for handling user interactions that will result in CRUD operations against the database.</p>
+    </div>
+
+    <a href="<?=PROOT?>documentation/models" class="btn btn-xs btn-secondary mb-5">Models</a>
+</div>
+<script src="<?=PROOT?>public/js/docNavDropdown.js"></script>
+<?php $this->end(); ?>
