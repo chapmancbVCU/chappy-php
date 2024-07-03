615722d0e7b6c8d01838ae0f927ed58fe617d117
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-03 18:33:14 -0400

    Added more settings to .env and updated .env.sample

--- config/config.php
+++ config/config.php
@@ -3,27 +3,27 @@
  * Configuration for mvc framework.
  */
 
-define('DEBUG', true);
-define('CONSOLE_LOGGING', true);                // JS console logging.
+define('DEBUG', $_ENV['DEBUG']);
+define('CONSOLE_LOGGING', $_ENV['CONSOLE_LOGGING']);        // JS console logging.
 
-define('DEFAULT_CONTROLLER', 'Home');           // Default controller if there isn't one defined in the URL.
-define('DEFAULT_LAYOUT', 'default');            // If no layout is set in the controller use this layout.
+define('DEFAULT_CONTROLLER', $_ENV['DEFAULT_CONTROLLER']);  // Default controller if there isn't one defined in the URL.
+define('DEFAULT_LAYOUT', $_ENV['DEFAULT_LAYOUT']);          // If no layout is set in the controller use this layout.
 
 define('DB_NAME', $_ENV['DB_NAME']);
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 define('DB_HOST', $_ENV['DB_HOST']);
 
-define('PROOT', '/custom-php-mvc-framework/');  // Set this to '/' for a live server.
+define('PROOT', $_ENV['PROOT']);                            // Set this to '/' for a live server.
 
-define('SITE_TITLE', 'MVC Framework');          // This will be used if no site title is set.
-define('MENU_BRAND', 'MVC Framework');       // Branding for menu.
+define('SITE_TITLE', $_ENV['SITE_TITLE']);                  // This will be used if no site title is set.
+define('MENU_BRAND', $_ENV['MENU_BRAND']);                  // Branding for menu.
 
-define('CURRENT_USER_SESSION_NAME', 'afEafFefafDFD');   // Session name for logged in user;
-define('REMEMBER_ME_COOKIE_NAME', 'ADF4R4affa43fFF');   // Cookie name for logged in user remember me
-define('REMEMBER_ME_COOKIE_EXPIRY', 2592000);   // Expire after 30 days, value in seconds
+define('CURRENT_USER_SESSION_NAME', $_ENV['CURRENT_USER_SESSION_NAME']);   // Session name for logged in user;
+define('REMEMBER_ME_COOKIE_NAME', $_ENV['REMEMBER_ME_COOKIE_NAME']);       // Cookie name for logged in user remember me
+define('REMEMBER_ME_COOKIE_EXPIRY', $_ENV['REMEMBER_ME_COOKIE_EXPIRY']);   // Expire after 30 days, value in seconds
 
-define('ACCESS_RESTRICTED', 'Restricted');      //Controller name for the restricted redirect.
+define('ACCESS_RESTRICTED', $_ENV['ACCESS_RESTRICTED']);    //Controller name for the restricted redirect.
 
 /* 
  *  ADD ADDITIONAL CONFIGURATION HERE.
