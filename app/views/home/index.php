46e773c886745b433bee5fd82ab65eaf1b36fcfd
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-03 18:15:06 -0400

    Tweaked styling for card borders

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -8,17 +8,15 @@
 
 <div class="d-flex flex-column w-75 mx-auto">
   <div class="d-flex flex-wrap justify-content-around">
-
-    <div class="card mb-5" style="width: 30rem;">
-      <img class="card-img-top" src="<?=PROOT?>public/images/form_helper.png" alt="Card image cap">
+    <div class="card mb-5 border border-primary rounded-lg" style="width: 30rem;">
+      <img class="card-img-top" src="<?=PROOT?>public/images/form_helper.png" alt="Example FormHelper function call">
       <div class="card-body">
         <h5 class="card-title">Rapid Form Development</h5>
         <p class="card-text">Built in library of functions focused on rapid development of responsive web forms.</p>
       </div>
     </div>
-
-    <div class="card mb-5" style="width: 30rem;">
-      <img class="card-img-top" src="<?=PROOT?>public/images/validation.png" alt="Card image cap">
+    <div class="card mb-5 border border-primary rounded-lg" style="width: 30rem;">
+      <img class="card-img-top" src="<?=PROOT?>public/images/validation.png" alt="Validation messages">
       <div class="card-body">
         <h5 class="card-title">Server Side Form Validation</h5>
         <p class="card-text">Built in suite of server side form validation tools with message alert support.</p>
@@ -27,15 +25,15 @@
   </div>
   
   <div class="d-flex flex-wrap justify-content-around">
-    <div class="card mb-5" style="width: 30rem;">
-      <img class="card-img-top" src="<?=PROOT?>public/images/contacts.png" alt="Card image cap">
+    <div class="card mb-5 border border-primary rounded-lg" style="width: 30rem;">
+      <img class="card-img-top" src="<?=PROOT?>public/images/contacts.png" alt="Contacts page">
       <div class="card-body">
         <h5 class="card-title">Sample MVC App</h5>
         <p class="card-text">The sample contact management systems demonstrates how to use all features of this MVC framework.</p>
       </div>
     </div>
-    <div class="card mb-5" style="width: 30rem;">
-      <img class="card-img-top" src="<?=PROOT?>public/images/register.png" alt="Card image cap">
+    <div class="card mb-5 border border-primary rounded-lg" style="width: 30rem;">
+      <img class="card-img-top" src="<?=PROOT?>public/images/register.png" alt="User registration page">
       <div class="card-body">
         <h5 class="card-title">User Management System</h5>
         <p class="card-text">Register and manage users with ease.  Use built in validation to enforce strong password requirements.</p>
@@ -44,16 +42,16 @@
   </div>
 
   <div class="d-flex flex-wrap justify-content-around">
-   <div class="card mb-5" style="width: 30rem;">
+   <div class="card mb-5 border border-primary rounded-lg" style="width: 30rem;">
       <img class="card-img-top" src="<?=PROOT?>public/images/documentation.png" alt="Card image cap">
       <div class="card-body">
         <h5 class="card-title">MVC Documentation</h5>
         <p class="card-text">Descriptions of all functions and classes that come with this MVC framework.</p>
       </div>
     </div>
-    <div class="card mb-5" style="width: 30rem;">
+    <div class="card mb-5 border border-primary rounded-lg" style="width: 30rem;">
       <img class="card-img-top" src="<?=PROOT?>public/images/ajax.png" alt="Card image cap">
-      <img class="card-img-top" src="<?=PROOT?>public/images/ajax_request.png" alt="Card image cap">
+      <img class="card-img-top" src="<?=PROOT?>public/images/ajax_request.png" alt="Ajax request code">
       <div class="card-body d-flex flex-column">
         <h5 class="card-title">Example Ajax Request</h5>
         <p class="card-text">Demonstration of an ajax requests.</p>
