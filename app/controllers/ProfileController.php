ec40064952c719065764c29b3c0b0f53aecdf5f1
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-05 02:31:19 -0400

    Added phpdoc comments

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -6,7 +6,7 @@ use App\Models\Users;
 use Core\Helper;
 
 /**
- * Undocumented class
+ * Supports ability to use user profile features and render relevant views.
  */
 class ProfileController extends Controller {
     /**
@@ -21,6 +21,11 @@ class ProfileController extends Controller {
         parent::__construct($controller, $action);
     }
 
+    /**
+     * Renders edit profile page and handles database updates.
+     *
+     * @return void
+     */
     public function editAction(): void {
         $user = Users::currentUser();
         if(!$user) Router::redirect('');
@@ -37,13 +42,23 @@ class ProfileController extends Controller {
         $this->view->render('profile/edit');
     }
 
+    /**
+     * Renders change profile image page.  Performs task of 
+     * processing file, file upload, and database update.
+     *
+     * @return void
+     */
     public function editProfileImageAction(): void {
         $user = Users::currentUser();
         if(!$user) Router::redirect('');
         if($this->request->isPost()) {
             $this->request->csrfCheck();
             $user->assign($this->request->get());
-            $fileTypes = ['png', 'jpg', 'gif', 'bmp'];
+
+            // Accepted file types.
+            $fileTypes = ['png', 'jpg', 'gif', 'bmp'];  
+
+            // Process file and set DB name
             $user->profileImage = $user->processFile($_FILES, "profileImage", $user->username, $user->profileImage, $fileTypes);
             if($user->save()) {
                 Router::redirect('profile/index');
@@ -55,33 +70,49 @@ class ProfileController extends Controller {
         $this->view->render('profile/edit_profile_image');
     }
 
+    /**
+     * Renders profile view for current logged in user.
+     *
+     * @return void
+     */
     public function indexAction(): void {
         $user = Users::currentUser();
-
-        if(!$user) {
-            Router::redirect('');
-        }
+        if(!$user) { Router::redirect(''); }
         $this->view->user = $user;
         $this->view->render('profile/index');
     }
 
+    /**
+     * Renders change password page.  Supports validation and database 
+     * operation.
+     *
+     * @return void
+     */
     public function updatePasswordAction(): void {
         $user = Users::currentUser();
         if(!$user) Router::redirect('');
         if($this->request->isPost()) {
             $this->request->csrfCheck();
             $user->assign($this->request->get());
+
+            // PW mode on for correct validation.
             $user->setChangePassword(true);
+
+            // Allows password matching confirmation.
             $user->setConfirm($this->request->get('confirm'));
+
             if($user->save()) {
+                // PW change mode off.
+                $user->setChangePassword(false);    
                 Router::redirect('profile/index');
-                $user->setChangePassword(false);
             }
         }
+
+        // PW change mode off and final page setup.
+        $user->setChangePassword(false);
         $this->view->displayErrors = $user->getErrorMessages();
         $this->view->user = $user;
         $this->view->postAction = PROOT . 'profile' . DS . 'update_password' . DS . $user->id;
         $this->view->render('profile/update_password');
     }
-
 }
