e3e8522f03f8fa0ecf49aa66531e0abf5f40fc2f
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-09 20:00:55 -0400

    Updated styling

--- app/views/userguide/validation.php
+++ app/views/userguide/validation.php
@@ -5,7 +5,7 @@
 <div class="main">
 <a href="<?=PROOT?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
     <h1 class="text-center">Validation</h1>
-    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded">
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
         Test
     </div>
 </div>
