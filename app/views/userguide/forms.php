1d9b270a3f54e6d6df460d227f52706184798b1d
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-13 17:54:55 -0400

    Added bootstrap class to keep button at same location

--- app/views/userguide/forms.php
+++ app/views/userguide/forms.php
@@ -3,8 +3,9 @@
 <?php include(ROOT . DS . 'app' . DS . 'views/layouts/docs_nav.php'); ?>
 
 <div class="main">
-<a href="<?=PROOT?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
-    <h1 class="text-center">Forms</h1>
+    <div class="position-fixed">
+        <a href="<?=PROOT?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
+    </div>
     <div class="mb-5 mt-3 my-5 w-75 bg-light mx-auto border rounded p-4">
         Test
     </div>
--- app/views/userguide/getting_started.php
+++ app/views/userguide/getting_started.php
@@ -3,7 +3,9 @@
 <?php include(ROOT . DS . 'app' . DS . 'views/layouts/docs_nav.php'); ?>
 
 <div class="main">
-<a href="<?=PROOT?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
+    <div class="position-fixed">
+        <a href="<?=PROOT?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
+    </div>
     <h1 class="text-center">Getting Started</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
         <ol class="pl-4">
