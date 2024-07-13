699f7d00403759e0d11c2df32fab294b7b586cd0
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-13 17:45:42 -0400

    Added content to overview section

--- app/views/userguide/contact_management.php
+++ app/views/userguide/contact_management.php
@@ -16,10 +16,28 @@
 
     <h1 id="overview" class="text-center">Overview</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        <p>The Contact Management System is a sample application that demonstrates how this Model 
+            View Controller (MVC) Framework operates.  The ContactsController class provides examples on how 
+            this framework supports Create, Read, Update, and Delete (CRUD) operations.
+        </p>
+
+        <p>The My Contacts view is controlled by the indexAction.  It demonstrates the ability to create 
+            lists of contacts from the contacts table and display some information for each record.  It also 
+            has a link to the details page by clicking a name for a particular contact.  There are also buttons 
+            for performing update and delete operations.
+        </p>
+
+        <p>An image of the contacts home page can be found below in Figure 1.</p>
+
+        <figure class="d-flex flex-column justify-content-center align-items-center">
+            <img class="img-fluid" src="<?=PROOT?>public/images/userGuide/contacts.png" alt="My Contacts Home">
+            <figcaption>Figure 1 - My Contacts Home</figcaption>
+        </figure>
     </div>
 
     <h1 id="details" class="text-center">Details</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        <p></p>
     </div>
 
     <h1 id="edit" class="text-center">Edit</h1>
