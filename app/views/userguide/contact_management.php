ca381e0d9f54ebb5b3d0774baeaf1300afecacb6
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-14 12:37:37 -0400

    Added content to details, edit, and delete

--- app/views/userguide/contact_management.php
+++ app/views/userguide/contact_management.php
@@ -39,15 +39,34 @@
 
     <h1 id="details" class="text-center">Details</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
-        <p></p>
+        <p>This view is accessed by clicking on a name of a contact on the My Contacts view.  It 
+            displays basic information for a contact.  The left side shows contact information 
+            and the right side shows your contact's address in the form of an address label.  Clicking 
+            on the back button will take you back to the My Contacts view.  
+            A screenshot of this view can be found below in figure 2.
+        </p>
+        <figure class="d-flex flex-column justify-content-center align-items-center">
+            <img class="img-fluid" src="<?=PROOT?>public/images/userGuide/details.png" alt="Contact details view">
+            <figcaption>Figure 2 - Contact Details View</figcaption>
+        </figure>
     </div>
 
     <h1 id="edit" class="text-center">Edit</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        <p>The Edit Contact view allows the use to update any contact.  Server-side validation ensures 
+            acceptable data is submitted to the database.  By default, front-end validation enforces 
+            proper formatting for the E-mail address and JavaScript is used to ensure phone numbers are 
+            in a consistent and correct format.
+        </p>
+        <figure class="d-flex flex-column justify-content-center align-items-center">
+            <img class="img-fluid" src="<?=PROOT?>public/images/userGuide/edit.png" alt="Edit Contact view">
+            <figcaption>Figure 3 - Edit Contact View</figcaption>
+        </figure>
     </div>
 
     <h1 id="delete" class="text-center">Delete</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        <p>When a user deletes a contact a confirmation prompt appears asking "Are you Sure?"</p>
     </div>
 </div>
 <script src="<?=PROOT?>public/js/docNavDropdown.js"></script>
