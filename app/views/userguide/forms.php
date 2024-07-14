69b3cb7fb168e50f036f4edef82ec2b2703ea119
Chad Chapman <chad.chapman2010@gmail.com>
2024-07-14 19:10:10 -0400

    Created scaffolding for forms user guide view

--- app/views/userguide/forms.php
+++ app/views/userguide/forms.php
@@ -3,11 +3,93 @@
 <?php include(ROOT . DS . 'app' . DS . 'views/layouts/docs_nav.php'); ?>
 
 <div class="main">
-    <div class="position-fixed">
-        <a href="<?=PROOT?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
+<div class="position-fixed">
+    <a href="<?=PROOT?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
     </div>
-    <div class="mb-5 mt-3 my-5 w-75 bg-light mx-auto border rounded p-4">
-        Test
+    <h1 class="text-center">Forms</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        <ol class="pl-4">
+            <li><a href="#overview">Overview</a></li>
+            <li><a href="#button">button</a></li>
+            <li><a href="#button-block"></a>buttonBlock</li>
+            <li><a href="#checkbox-block-label-left">checkboxBlockLabelLeft</a></li>
+            <li><a href="#checkbox-block-label-right">checkboxBlockLabelRight</a></li>
+            <li><a href="#csrf-input">csrfInput</a></li>
+            <li><a href="#display-errors">displayErrors</a></li>
+            <li><a href="#email-block"></a>emailBlock</li>
+            <li><a href="#hidden">hidden</a></li>
+            <li><a href="#input-block">inputBlock</a></li>
+            <li><a href="#output">output</a></li>
+            <li><a href="#radio-input">radioInput</a></li>
+            <li><a href="#select-block">selectBlock</a></li>
+            <li><a href="#submit-tag">submitTag</a></li>
+            <li><a href="#tel-block">telBlock</a></li>
+            <li><a href="#textarea-block">textAreaBlock</a></li>
+        </ol>
+    </div>
+
+    <h1 id="overview" class="text-center">Overview</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="button" class="text-center">button</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="button-block" class="text-center">buttonBlock</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="checkbox-block-label-left" class="text-center">checkboxBlockLabelLeft</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="checkbox-block-label-right" class="text-center">checkboxBlockLabelRight</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="csrf-input" class="text-center">csrfInput</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="display-errors" class="text-center">displayErrors</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="email-block" class="text-center">emailBlock</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="hidden" class="text-center">hidden</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="input-block" class="text-center">inputBlock</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="output" class="text-center">output</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="radio-input" class="text-center">radioInput</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="select-block" class="text-center">selectBlock</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="submit-block" class="text-center">submitBlock</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="submit-tag" class="text-center">submitTag</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+    </div>
+
+    <h1 id="textarea-block" class="text-center">textAreaBlock</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
     </div>
 </div>
 <script src="<?=PROOT?>public/js/docNavDropdown.js"></script>
