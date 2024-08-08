a77ad514a2331f40de122028fc7e31bc1116f17b
Chad Chapman <chad.chapman2010@gmail.com>
2024-08-07 21:21:45 -0400

    Initial setup of index action to get list of users

--- app/controllers/AdmindashboardController.php
+++ app/controllers/AdmindashboardController.php
@@ -1,12 +1,18 @@
 <?php
 namespace App\Controllers;
 use Core\Controller;
-
+use App\Models\Users;
+use Core\Helper;
 /**
  * Implements support for our Home controller.  Functions found in this class 
  * will support tasks related to the home page.
  */
 class AdmindashboardController extends Controller {
+    public function onConstruct()
+    {
+        $this->view->setLayout('admin');
+        $this->currentUser = Users::currentUser();
+    }
     /** 
      * The default action for this controller.  It performs rendering of this 
      * site's home page.
@@ -14,7 +20,9 @@ class AdmindashboardController extends Controller {
      * @return void
      */
     public function indexAction(): void {
-        echo "hi";
-        //$this->view->render('admindashboard/index');
+        $users = Users::findAllUsers($this->currentUser->id);
+        Helper::cl($users);
+        $this->view->users = $users;
+        $this->view->render('admindashboard/index');
     }
 }
