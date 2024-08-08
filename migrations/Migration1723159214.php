08380b919d78477181a349c6762556473136d807
Chad Chapman <chad.chapman2010@gmail.com>
2024-08-08 19:26:00 -0400

    Added migration to create acl table

---
+++ migrations/Migration1723159214.php
@@ -0,0 +1,12 @@
+<?php
+namespace Migrations;
+use Core\Migration;
+
+class Migration1723159214 extends Migration {
+  public function up() {
+    $table = 'acl';
+    $this->createTable($table);
+    $this->addColumn($table, 'acl', 'varchar', ['size' => 25]);
+    $this->addSoftDelete($table);
+  }
+}
