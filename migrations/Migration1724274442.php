44c88e56d4644eea7d955f37a83f46c39f75624e
Chad Chapman <chad.chapman2010@gmail.com>
2024-08-21 17:17:57 -0400

    Added migration for profile images table

---
+++ migrations/Migration1724274442.php
@@ -0,0 +1,14 @@
+<?php
+namespace Migrations;
+use Core\Migration;
+
+class Migration1724274442 extends Migration {
+  public function up() {
+    $table = 'profile_images';
+    $this->createTable($table);
+    $this->addColumn($table, 'url', 'varchar', ['size' => 255]);
+    $this->addColumn($table, 'user_id', 'int');
+    $this->addColumn($table, 'name', 'varchar', ['size' => 255]);
+    $this->addSoftDelete($table);
+  }
+}
