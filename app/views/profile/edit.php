bba51d4e4b9387ed8f2f8c54468d64014a05eef9
Chad Chapman <chad.chapman2010@gmail.com>
2024-08-25 16:58:08 -0400

    Moved js for sorting images to separate file

--- app/views/profile/edit.php
+++ app/views/profile/edit.php
@@ -50,39 +50,5 @@ use Core\FormHelper;
         </form>
     </div>
 </div>
-
-<script>
-    function updateSort() {
-        var sortedIDs = $("#sortableImages").sortable("toArray");
-        $('#images_sorted').val(JSON.stringify(sortedIDs));
-    }
-
-    function deleteImage(image_id) {
-        if(confirm("Are you sure?  This cannot be undone!")) {
-            jQuery.ajax({
-               url : '<?=APP_DOMAIN?>profile/deleteImage',
-               method: "POST",
-               data : {image_id : image_id},
-               success: function(resp) {
-                    if(resp.success) {
-                        jQuery('#image_'+resp.model_id).remove();
-                        updateSort();
-                        alertMsg('Image Deleted.');
-                    }
-                } 
-            });
-        }
-    }
-
-    jQuery('document').ready(function(){
-        jQuery('#sortableImages').sortable({
-            axis: "x",
-            placeholder: "sortable-placeholder",
-            update : function(event, ui) {
-                updateSort();
-            }
-        });
-        updateSort();
-    });
-</script>
+<script type="text/javascript" src="<?=APP_DOMAIN?>public/js/manageProfileImages.js"></script>
 <?php $this->end(); ?>
