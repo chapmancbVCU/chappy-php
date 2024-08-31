abec7bbcb0ed8e4546d23b4b15c52cb92081c675
Chad Chapman <chad.chapman2010@gmail.com>
2024-08-31 19:21:37 -0400

    Added include setup-composer.php

---
+++ src/init/setup-composer.php
@@ -0,0 +1,6 @@
+<?php
+
+shell_exec('composer require vlucas/phpdotenv');
+shell_exec('composer require tinymce/tinymce');
+shell_exec('composer require symfony/console');
+shell_exec('composer require symfony/filesystem');
