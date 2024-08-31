15298e3a8c950ef3fbd772f6d97d430a04726972
Chad Chapman <chad.chapman2010@gmail.com>
2024-08-31 19:00:29 -0400

    Implemented migration feature

--- src/console/tools/RunMigrationCommand.php
+++ src/console/tools/RunMigrationCommand.php
@@ -1,6 +1,6 @@
 <?php
 namespace Console\App\Commands;
- 
+use Core\DB;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -31,21 +31,50 @@ class RunMigrationCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        if (php_sapi_name() != 'cli') die('Restricted');
-        $fileName = "Migration".time();
-        $ext = ".php";
-        $fullPath = CONSOLE_ROOT.DS.'migrations'.DS.$fileName.$ext;
-        $content = '<?php
-namespace Migrations;
-use Core\Migration;
+        
+        // load configuration and helper functions
+        require_once(CONSOLE_ROOT . DS . 'config' . DS . 'config.php');
+        $isCli = php_sapi_name() == 'cli';
+        if(!RUN_MIGRATIONS_FROM_BROWSER && !$isCli) die('restricted');
 
-class '.$fileName.' extends Migration {
-    public function up() {
+        
 
-    }
-}
-';
-        $resp = file_put_contents($fullPath, $content);
+       
+
+        $db = DB::getInstance();
+
+        $migrationTable = $db->query("SHOW TABLES LIKE 'migrations'")->results();
+        $previousMigs = [];
+        $migrationsRun = [];
+
+        if(!empty($migrationTable)){
+            $query = $db->query("SELECT migration FROM migrations")->results();
+            foreach($query as $q){
+                $previousMigs[] = $q->migration;
+            }
+        }
+        // get all files
+        $migrations = glob('migrations'.DS.'*.php');
+
+        foreach($migrations as $fileName){
+            $klass = str_replace('migrations'.DS,'',$fileName);
+            $klass = str_replace('.php','',$klass);
+            if(!in_array($klass,$previousMigs)){
+                $klassNamespace = 'Migrations\\'.$klass;
+                $mig = new $klassNamespace($isCli);
+                $mig->up();
+                $db->insert('migrations',['migration'=>$klass]);
+                $migrationsRun[] = $klassNamespace;
+            }
+        }
+
+        if(sizeof($migrationsRun) == 0){
+            if($isCli){
+                echo "\e[0;37;42m\n\n"."    No new migrations to run.\n\e[0m\n";
+            } else {
+            echo '<p style="color:#006600;">No new migrations to run.</p>';
+            }
+        }
 
         return Command::SUCCESS;
     }
