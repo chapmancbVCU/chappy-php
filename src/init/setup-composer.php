84cc1a4a0ceb1947026a4d172dc03b386d395d4c
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-01 14:57:47 -0400

    Test

--- src/init/setup-composer.php
+++ src/init/setup-composer.php
@@ -1,6 +1,2 @@
 <?php
-
-shell_exec('composer require vlucas/phpdotenv');
-shell_exec('composer require tinymce/tinymce');
-shell_exec('composer require symfony/console');
-shell_exec('composer require symfony/filesystem');
+shell_exec('composer install');
