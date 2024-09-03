a05b07e95202352ab27fb584e02651779a7a35c1
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-03 14:23:07 -0400

    Updated if statement

--- core/Application.php
+++ core/Application.php
@@ -19,7 +19,7 @@ class Application {
      * @return void
      */
     private function _set_reporting(): void {
-        if(DEBUG) {
+        if(DEBUG == "true") {
             // error_reporting(E_ALL);
             error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
             ini_set('display_errors', 1);
