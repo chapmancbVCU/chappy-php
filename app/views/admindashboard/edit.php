ce6a2bcebfdf4913c0c2bc89d70c4d929a5674af
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-04 11:25:20 -0400

    Added support for profile images

--- app/views/admindashboard/edit.php
+++ app/views/admindashboard/edit.php
@@ -5,8 +5,11 @@ use Core\Helper;
 ?>
 <?php $this->setSiteTitle("Edit Details for ".$this->user->username); ?>
 <?php $this->start('head') ?>
+    <link rel="stylesheet" href="<?=APP_DOMAIN?>public/css/profileImage.css?v=<?=VERSION?>" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="<?=APP_DOMAIN?>public/css/jquery-ui/jquery-ui.min.css">
     <script src='<?=APP_DOMAIN?>vendor/tinymce/tinymce/tinymce.min.js'></script>
     <script src='<?=APP_DOMAIN?>public/js/profileDescriptionTinyMCE.js'></script>
+    <script type="text/javascript" src="<?=APP_DOMAIN?>public/js/jquery-ui/jquery-ui.min.js"></script>
 <?php $this->end() ?>
 <?php $this->start('body'); ?>
 <div class="row align-items-center justify-content-center">
@@ -15,6 +18,7 @@ use Core\Helper;
         <hr>
         <form class="form" action=<?=$this->postAction?> method="post">
             <?= FormHelper::csrfInput() ?>
+            <input type="hidden" id="images_sorted" name="images_sorted" value="" />
             <?= FormHelper::inputBlock('text', "First Name", 'fname', $this->user->fname, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
             <?= FormHelper::inputBlock('text', "Last Name", 'lname', $this->user->lname, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
             <?= FormHelper::emailBlock("Email", 'email', $this->user->email, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
@@ -27,7 +31,19 @@ use Core\Helper;
             ?>
 
             <?= FormHelper::selectBlock('ACL', 'acl', $this->aclId, $this->acls, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors); ?>
+            <?= FormHelper::inputBlock('file', "Upload Profile Image (Optional)", 'profileImage', '', ['class' => 'form-control', 'accept' => 'image/png image/jpeg image/png'], ['class' => 'form-group w-50'], $this->displayErrors) ?>
             
+            <div id="sortableImages" class="row align-items-center justify-content-start p-2">
+                <?php foreach($this->profileImages as $image):?>
+                    <div class="col flex-grow-0" id="image_<?=$image->id?>">
+                        <span class="delete-button" onclick="deleteImage('<?=$image->id?>')"><i class="fa fa-times"></i></span>
+                        <div class="edit-image-wrapper <?= ($image->sort == 0) ? 'current-profile-img' : ''?>" data-id="<?=$image->id?>">
+                            <img src="<?=APP_DOMAIN.$image->url?>" />
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
             <div class="col-md-12 text-right">
                 <a href="<?=APP_DOMAIN?>admindashboard/details/<?=$this->user->id?>" class="btn btn-default">Cancel</a>
                 <?= FormHelper::submitTag('Update', ['class' => 'btn btn-primary'])  ?>
@@ -35,4 +51,40 @@ use Core\Helper;
         </form>
     </div>
 </div>
+
+<script>
+    function updateSort() {
+        var sortedIDs = $("#sortableImages").sortable("toArray");
+        $('#images_sorted').val(JSON.stringify(sortedIDs));
+    }
+
+    function deleteImage(image_id) {
+        if(confirm("Are you sure?  This cannot be undone!")) {
+            jQuery.ajax({
+                url : '<?=APP_DOMAIN?>profile/deleteImage',
+                method: "POST",
+                data : {image_id : image_id},
+                success: function(resp) {
+                    if(resp.success) {
+                        jQuery('#image_'+resp.model_id).remove();
+                        updateSort();
+                        alertMsg('Image Deleted.');
+                    }
+                } 
+            });
+        }
+    }
+
+    jQuery('document').ready(function(){
+        jQuery('#sortableImages').sortable({
+            axis: "x",
+            placeholder: "sortable-placeholder",
+            update : function(event, ui) {
+                updateSort();
+            }
+        });
+        updateSort();
+    });
+</script>
+
 <?php $this->end(); ?>
