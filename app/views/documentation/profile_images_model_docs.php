8c080ccd059d8e4eea5412b7a2aa2150c13eb547
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-04 13:27:48 -0400

    Added view for ProfileImages model docs

---
+++ app/views/documentation/profile_images_model_docs.php
@@ -0,0 +1,15 @@
+<?php $this->setSiteTitle("Profile Images Model - Custom MVC Framework Docs"); ?>
+<?php $this->start('body'); ?>
+
+<div class="main">
+    <a href="<?=APP_DOMAIN?>documentation/models" class="btn btn-xs btn-secondary">Models</a>
+    <h1 class="text-center">ProfileImages Model Class</h1>
+    <div class="row align-items-center justify-content-center my-3">
+        <p class="text-center w-75">Add description here</p>
+    </div>
+
+    <a href="<?=APP_DOMAIN?>documentation/models" class="btn btn-xs btn-secondary mb-5">Models</a>
+</div>
+
+<script src="<?=APP_DOMAIN?>public/js/docNavDropdown.js"></script>
+<?php $this->end(); ?>
