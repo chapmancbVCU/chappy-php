202d954282a6a8a5b6755dc4c45911ff36ce1117
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-04 14:42:29 -0400

    Imitial commit

---
+++ app/views/admindashboard/add_acl.php
@@ -0,0 +1,6 @@
+<?php use Core\Helper; ?>
+<?php $this->setSiteTitle("Add ACL"); ?>
+<?php $this->start('body'); ?>
+<h1 class="text-center">Add ACL</h1>
+
+<?php $this->end(); ?>
---
+++ app/views/admindashboard/edit_acl.php
@@ -0,0 +1,6 @@
+<?php use Core\Helper; ?>
+<?php $this->setSiteTitle("Edit ACL"); ?>
+<?php $this->start('body'); ?>
+<h1 class="text-center">Edit ACL</h1>
+
+<?php $this->end(); ?>
