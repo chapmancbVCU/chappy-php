e8e6885ac2240385b217d520f3455d1d538cf8b5
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-04 15:58:44 -0400

    Added acl name

--- app/views/admindashboard/edit_acl.php
+++ app/views/admindashboard/edit_acl.php
@@ -1,6 +1,6 @@
 <?php use Core\Helper; ?>
-<?php $this->setSiteTitle("Edit ACL"); ?>
+<?php $this->setSiteTitle("Edit ACL - " . $this->acl->acl); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center">Edit ACL</h1>
+<h1 class="text-center">Edit ACL - <?= $this->acl->acl ?></h1>
 
 <?php $this->end(); ?>
