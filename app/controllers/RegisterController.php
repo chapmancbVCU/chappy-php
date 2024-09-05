8ae56876b46008105bf6537b7522a9079a29abcd
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-05 19:18:10 -0400

    User constant for max login attempts variable

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -17,7 +17,6 @@ class RegisterController extends Controller {
      */
     public function loginAction(): void {
         $loginModel = new Login();
-        $maxLoginAttempts = 5;
         if($this->request->isPost()) {
             // form validation
             $this->request->csrfCheck();
@@ -41,11 +40,11 @@ class RegisterController extends Controller {
                 }  else {
                     if($user) {
                         $user->login_attempts = $user->login_attempts + 1;
-                        if($user->login_attempts > $maxLoginAttempts) {
+                        if($user->login_attempts >= MAX_LOGIN_ATTEMPTS) {
                             $user->inactive = 1;
                         }
                         if($user->save()) {
-                            if($user->login_attempts > 0 && $user->login_attempts < $maxLoginAttempts) {
+                            if($user->login_attempts > 0 && $user->login_attempts < MAX_LOGIN_ATTEMPTS) {
                                 $loginModel->addErrorMessage('username', 'There is an error with your username or password.');
                             } else {
                                 $loginModel->addErrorMessage('username', 'Your account has been locked due to too many failed login attempts.');
