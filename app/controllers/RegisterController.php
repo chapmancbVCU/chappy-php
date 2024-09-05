6dcf07bffc3da862176c09821b4fa44a5d259411
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-05 19:31:00 -0400

    Removed if statement

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -43,12 +43,11 @@ class RegisterController extends Controller {
                         if($user->login_attempts >= MAX_LOGIN_ATTEMPTS) {
                             $user->inactive = 1;
                         }
-                        if($user->save()) {
-                            if($user->login_attempts > 0 && $user->login_attempts < MAX_LOGIN_ATTEMPTS) {
-                                $loginModel->addErrorMessage('username', 'There is an error with your username or password.');
-                            } else {
-                                $loginModel->addErrorMessage('username', 'Your account has been locked due to too many failed login attempts.');
-                            }
+                        $user->save();
+                        if($user->login_attempts > 0 && $user->login_attempts < MAX_LOGIN_ATTEMPTS) {
+                            $loginModel->addErrorMessage('username', 'There is an error with your username or password.');
+                        } else {
+                            $loginModel->addErrorMessage('username', 'Your account has been locked due to too many failed login attempts.');
                         }
                     }
                     else {
