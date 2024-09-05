83d3d0400789eef0f78bac5036e90a9cc6d62ce3
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-05 13:54:45 -0400

    Implemented view

--- app/views/admindashboard/add_acl.php
+++ app/views/admindashboard/add_acl.php
@@ -3,4 +3,10 @@
 <?php $this->start('body'); ?>
 <h1 class="text-center">Add ACL</h1>
 
+<div class="row align-items-center justify-content-center">
+    <div class="col-md-6 bg-light p-3">
+        <?php $this->addPartialView('admindashboard', 'acl'); ?>
+    </div>
+</div>
+
 <?php $this->end(); ?>
