326c36db440492b3d10aee4f29c4f5eba6fcc525
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-14 19:55:38 -0400

    Moved password complexity requirements to new shared view

--- app/views/profile/update_password.php
+++ app/views/profile/update_password.php
@@ -6,19 +6,8 @@ use Core\FormHelper;
 <div class="row align-items-center justify-content-center">
     <div class="col-md-6 bg-light p-3">
         <h1 class="text-center">Change Password for <?=$this->user->username?></h1>
-        <hr>
-        <div>
-            <h4>Password Requirements</h4>
-            <ul class="pl-3">
-                <li>Minimum 12 characters in length</li>
-                <li>Maximum of 30 characters in length</li>
-                <li>At least 1 upper case character</li>
-                <li>At least 1 lower case character</li>
-                <li>At least 1 number</li>
-                <li>Must contain at least 1 special character</li>
-                <li>Must not contain any spaces</li>
-            </ul>
-        </div>
+        <hr>  
+        <?php $this->addPartialView('shared', 'password_complexity_requirements'); ?>
         <form class="form" action="" method="post">
             <?= FormHelper::csrfInput() ?>
             <?= FormHelper::displayErrors($this->displayErrors) ?>
--- app/views/register/reset_password.php
+++ app/views/register/reset_password.php
@@ -7,19 +7,8 @@ use Core\Helper;
 
 <div class="row align-items-center justify-content-center">
     <div class="col-md-6 bg-light p-3">
-    <h3 class="text-center">Reset Password</h3>
-        <div>
-            <h4>Password Requirements</h4>
-            <ul class="pl-3">
-                <li>Minimum 12 characters in length</li>
-                <li>Maximum of 30 characters in length</li>
-                <li>At least 1 upper case character</li>
-                <li>At least 1 lower case character</li>
-                <li>At least 1 number</li>
-                <li>Must contain at least 1 special character</li>
-                <li>Must not contain any spaces</li>
-            </ul>
-        </div>
+        <h3 class="text-center">Reset Password</h3>
+        <?php $this->addPartialView('shared', 'password_complexity_requirements'); ?>
         <form class="form" action="" method="post">
             <?= FormHelper::csrfInput() ?>
             <?= FormHelper::displayErrors($this->displayErrors) ?>
