0ee63b465aef6a5aeffc55b264e7d8ab0dec2255
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-14 19:56:22 -0400

    Shared pw requirement partial with logic to render specific requirements based on env variables.

---
+++ app/views/shared/partials/password_complexity_requirements.php
@@ -0,0 +1,32 @@
+<div>
+    <h4>Password Requirements</h4>
+    <ul class="pl-3">
+        <?php if(SET_PW_MIN_LENGTH == "true"):?>
+            <li>Minimum 12 characters in length</li>
+        <?php endif; ?>
+
+        <?php if(SET_PW_MAX_LENGTH == "true"):?>
+            <li>Maximum of 30 characters in length</li>
+        <?php endif; ?>
+
+        <?php if(PW_UPPER_CHAR == "true"):?>
+            <li>At least 1 upper case character</li>
+        <?php endif; ?>
+
+        <?php if(PW_LOWER_CHAR == "true"):?>
+            <li>At least 1 lower case character</li>
+        <?php endif; ?>
+
+        <?php if(PW_NUM_CHAR == "true"):?>
+            <li>At least 1 number</li>
+        <?php endif; ?>
+
+        <?php if(PW_SPECIAL_CHAR == "true"):?>
+            <li>Must contain at least 1 special character</li>
+        <?php endif; ?>
+
+        <?php if(SET_PW_MAX_LENGTH == "true"):?>   
+            <li>Must not contain any spaces</li>
+        <?php endif; ?>
+    </ul>
+</div>
