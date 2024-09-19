b6f42774a936ab74814043a091d9f5740e4d10e2
Chad Chapman <chad.chapman2010@gmail.com>
2024-09-18 20:35:23 -0400

    Initial commit, set title, and initial test to verify views work

--- app/controllers/UserguideController.php
+++ app/controllers/UserguideController.php
@@ -3,10 +3,22 @@ namespace App\Controllers;
 use Core\Controller;
 
 class UserguideController extends Controller {
+    public function aclAction(): void {
+        $this->view->render('userguide/acl');
+    }
+    public function administrationAction(): void {
+        $this->view->render('userguide/administration');
+    }
+    public function consoleAction(): void {
+        $this->view->render('userguide/console');
+    } 
     public function contactManagementAction(): void {
         $this->view->render('userguide/contact_management');
     }
 
+    public function controllersAction(): void {
+        $this->view->render('userguide/controllers');
+    }
     public function formsAction(): void {
         $this->view->render('userguide/forms');
     } 
@@ -25,8 +37,16 @@ class UserguideController extends Controller {
         $this->view->render('userguide/index');
     }
 
+    public function javascriptAction(): void {
+        $this->view->render('userguide/javascript');
+    }
+
     public function loginSystemAction(): void {
         $this->view->render('userguide/login_system');
+    }
+
+    public function modelsAction(): void {
+        $this->view->render('userguide/models');
     }
 
     /**
@@ -38,6 +58,17 @@ class UserguideController extends Controller {
         $this->view->setLayout('docs');
     }
 
+    public function phpunitAction(): void {
+        $this->view->render('userguide/phpunit');
+    }
+
+    public function sessionMessagesAction(): void {
+        $this->view->render('userguide/session_messages');
+    }
+
+    public function uploadsAction(): void {
+        $this->view->render('userguide/uploads');
+    }
     public function userProfilesAction(): void {
         $this->view->render('userguide/user_profiles');
     }
@@ -46,4 +77,7 @@ class UserguideController extends Controller {
         $this->view->render('userguide/validation');
     }
     
+    public function viewsAction(): void {
+        $this->view->render('userguide/views');
+    }
 }
---
+++ app/views/userguide/controllers.php
@@ -0,0 +1,13 @@
+<?php $this->setSiteTitle("Controllers - User Guide"); ?>
+<?php $this->start('body'); ?>
+
+<div class="main">
+    <div class="position-fixed">
+        <a href="<?=APP_DOMAIN?>userguide/index" class="btn btn-xs btn-secondary">User Guide Home</a>
+    </div>
+    <h1 class="text-center">Controllers</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        Test
+    </div>
+</div>
+<?php $this->end(); ?>
