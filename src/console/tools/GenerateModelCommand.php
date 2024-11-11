ea5b0591b7d532c72598a41217c92247e343a6f3
Chad Chapman <chad.chapman2010@gmail.com>
2024-11-11 08:25:04 -0500

    Updated output of command

--- src/console/tools/GenerateModelCommand.php
+++ src/console/tools/GenerateModelCommand.php
@@ -42,10 +42,45 @@ namespace App\Models;
 use Core\Model;
 
 /**
- * Undocumented class
+ * 
  */
 class '.$modelName.' extends Model {
+
+    // Fields you don\'t want saved on form submit
+    // public const blackList = [];
+
+    // Set to name of database table.
+    protected static $_table = \''.lcfirst($modelName).'\';
+
+    // Soft delete
+    // protected static $_softDelete = true;
     
+    // Fields from your database
+
+    public function afterDelete(): void {
+        //
+    }
+
+    public function afterSave(): void {
+        //
+    }
+
+    public function beforeDelete(): void {
+        //
+    }
+
+    public function beforeSave(): void {
+        //
+    }
+
+    /**
+     * Performs validation for the '.$modelName.' model.
+     *
+     * @return void
+     */
+    public function validator(): void {
+        //
+    }
 }
 ';
         if(!file_exists($fullPath)) {
