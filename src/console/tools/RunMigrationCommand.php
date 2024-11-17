4e53c14b031ffcd31d5c44470c9a65383517b4af
Chad Chapman <chad.chapman2010@gmail.com>
2024-11-17 13:23:54 -0500

    MOved code for migration in execut function to its own class for resusability purposes in future dev efforts

--- src/console/tools/RunMigrationCommand.php
+++ src/console/tools/RunMigrationCommand.php
@@ -4,7 +4,7 @@ use Core\DB;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
+use Console\App\Helpers\Migrate;
 /**
  * Supports ability to run a migration file.
  */
@@ -31,46 +31,6 @@ class RunMigrationCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // Load configuration and helper functions
-       // require_once(ROOT . DS . 'config' . DS . 'database.php');
-        $isCli = php_sapi_name() == 'cli';
-        //if(!RUN_MIGRATIONS_FROM_BROWSER && !$isCli) die('restricted');
-
-        $db = DB::getInstance();
-        $migrationTable = $db->query("SHOW TABLES LIKE 'migrations'")->results();
-        $previousMigs = [];
-        $migrationsRun = [];
-
-        if(!empty($migrationTable)){
-            $query = $db->query("SELECT migration FROM migrations")->results();
-            foreach($query as $q){
-                $previousMigs[] = $q->migration;
-            }
-        }
-        
-        // get all files
-        $migrations = glob('migrations'.DS.'*.php');
-
-        foreach($migrations as $fileName){
-            $klass = str_replace('migrations'.DS,'',$fileName);
-            $klass = str_replace('.php','',$klass);
-            if(!in_array($klass,$previousMigs)){
-                $klassNamespace = 'Migrations\\'.$klass;
-                $mig = new $klassNamespace($isCli);
-                $mig->up();
-                $db->insert('migrations',['migration'=>$klass]);
-                $migrationsRun[] = $klassNamespace;
-            }
-        }
-
-        if(sizeof($migrationsRun) == 0){
-            if($isCli){
-                echo "\e[0;37;42m\n\n"."    No new migrations to run.\n\e[0m\n";
-            } else {
-            echo '<p style="color:#006600;">No new migrations to run.</p>';
-            }
-        }
-
-        return Command::SUCCESS;
+        return Migrate::migrate($input, $output);
     }
 }
