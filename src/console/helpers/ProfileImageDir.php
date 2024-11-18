44cfb848aa852e6f7487d2e612a42b6b52f1b3bf
Chad Chapman <chad.chapman2010@gmail.com>
2024-11-17 20:43:03 -0500

    Set path to profile images as static instance variable

--- src/console/helpers/ProfileImageDir.php
+++ src/console/helpers/ProfileImageDir.php
@@ -1,16 +1,16 @@
 <?php
 namespace Console\App\Helpers;
 
-use Core\DB;
 use RecursiveIteratorIterator;
 use RecursiveDirectoryIterator;
 use Symfony\Component\Console\Command\Command;
 
 class ProfileImageDir {
+    private static $_path = ROOT.DS.'public'.DS.'images'.DS.'uploads'.DS.'profile_images';
+
     public static function mkdirProfileImages(): int {
-        $path = ROOT.DS.'public'.DS.'images'.DS.'uploads'.DS.'profile_images';
-        if(!file_exists($path)) {
-            mkdir($path, 0775, true);
+        if(!file_exists(self::$_path)) {
+            mkdir(self::$_path, 0775, true);
         } else {
             echo "\e[0;37;42m\n\n"."    The profile_images directory already exists.\n\e[0m\n";
             return Command::FAILURE;
@@ -19,8 +19,7 @@ class ProfileImageDir {
     }
 
     public static function rmdirProfileImageDirectories(): int {
-        $dir = ROOT.DS.'public'.DS.'images'.DS.'uploads'.DS .'profile_images'.DS;
-        $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+        $it = new RecursiveDirectoryIterator(self::$_path, RecursiveDirectoryIterator::SKIP_DOTS);
         $files = new RecursiveIteratorIterator($it,
                      RecursiveIteratorIterator::CHILD_FIRST);
         foreach($files as $file) {
