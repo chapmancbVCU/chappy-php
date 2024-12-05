ac02772b536aa5b178b78793f3376b92c607bcad
Chad Chapman <chad.chapman2010@gmail.com>
2024-12-05 18:34:51 -0500

    Added table to summarize available commands

--- app/views/userguide/console.php
+++ app/views/userguide/console.php
@@ -9,6 +9,7 @@
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
         <ol class="pl-4">
             <li><a href="#overview">Overview</a></li>
+            <li><a href="#summary">Summary of Available Commands</a></li>
         </ol>
     </div>
 
@@ -29,6 +30,75 @@
             in this user guide.
         </p>
         <p>If there is a command you would like for us to support you can submit an issue <a href="https://github.com/chapmancbVCU/chappy-php/issues">here</a>.</p>
+    </div>
+
+    <h1 id="summary" class="text-center">Summary of Available Commands</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        <p>Below is a list of available commands.  Most items in this list contains a link to the page that describes an individual command.</p>
+        <table class="table table-striped table-condensed table-bordered table-hover mx-auto table-sm my-2">
+            <thead>
+                <th class="w-25 text-center">Command</th>
+                <th class="text-center">Description</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>migrate</td>
+                    <td>Runs a Database Migration!</td>
+                </tr>
+                <tr>
+                    <td>test</td>
+                    <td>Performs the phpunit test.</td>
+                </tr>
+                <tr>
+                    <td>init:mk-profile-images-dir</td>
+                    <td>Builds Profile Image Dir</td>
+                </tr>
+                <tr>
+                    <td>make:command</td>
+                    <td>Generates a new command class</td>
+                </tr>
+                <tr>
+                    <td>make:controller</td>
+                    <td>Generates a new controller file!</td>
+                </tr>
+                <tr>
+                    <td>make:db</td>
+                    <td>Creates a new database</td>
+                </tr>
+                <tr>
+                    <td>make:db-user</td>
+                    <td>Create a new database user (Coming soon!)</td>
+                </tr>
+                <tr>
+                    <td>make:migration</td>
+                    <td>Generates a Database Migration!</td>
+                </tr>
+                <tr>
+                    <td>make:model</td>
+                    <td>Generates a new model file!</td>
+                </tr>
+                <tr>
+                    <td>make:test</td>
+                    <td>Generates a new test file!</td>
+                </tr>
+                <tr>
+                    <td>migrate:drop-all</td>
+                    <td>Drops all database tables</td>
+                </tr>
+                <tr>
+                    <td>migrate:refresh</td>
+                    <td>Drops all tables and runs a Database Migration!</td>
+                </tr>
+                <tr>
+                    <td>tools:mk-env</td>
+                    <td>Creates the .env file</td>
+                </tr>
+                <tr>
+                    <td>tools:rm-profile-images</td>
+                    <td>Removes all profile images.</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 <?php $this->end(); ?>
