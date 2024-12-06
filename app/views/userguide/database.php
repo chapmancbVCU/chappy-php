88ba3bd4047cf2d542d0c7d3405a6a8dc464e6f0
Chad Chapman <chad.chapman2010@gmail.com>
2024-12-05 19:09:37 -0500

    Added section describing migrations

--- app/views/userguide/database.php
+++ app/views/userguide/database.php
@@ -23,7 +23,28 @@
     <h1 id="migration" class="text-center">Migration</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
         <p>Performing a database migration is the first task you will perform after establishing a new project.  
+            Before you begin you will need to open the .env file and enter some information about the database.  
+            An example is shown below:
         </p>
+<pre class="my-3 pb-1">
+<code class="language-php line-numbers">DB_NAME=chappy
+DB_USER=root
+DB_PASSWORD=asecurepassword
+DB_HOST='127.0.0.1'
+</code>
+</pre>
+        <p>Next, create the database.  If your database is on your local host you can run the make:db command.  
+            An example is shown below:
+        </p>
+        <pre class="my-3 pb-1">
+<code class="language-php line-numbers">php console make:db chappy
+</code>
+</pre>
+        <p>Finally, you can run the migrate command shown below:</p>
+        <pre class="my-3 pb-1">
+<code class="language-php line-numbers">php console migrate
+</code>
+</pre>
     </div>
 </div>
 <?php $this->end(); ?>
