f1dc6120042c94c56c6641272b9d3d8fdb58dbf9
Chad Chapman <chad.chapman2010@gmail.com>
2024-12-06 20:34:54 -0500

    Added intro

--- app/views/userguide/controllers.php
+++ app/views/userguide/controllers.php
@@ -8,9 +8,20 @@
     <h1 class="text-center">Controllers</h1>
     <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
         <ol class="pl-4">
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
+            <li><a href="#overview">Overview</a></li>
+            <li><a href="#controller-file">Controller File</a></li>
         </ol>
+    </div>
+
+    <h1 id="overview" class="text-center">Overview</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+        This framework supports controllers to manage the interactions between views 
+        and models.
+    </div>
+
+    <h1 id="controller-file" class="text-center">Controller File</h1>
+    <div class="mb-5 mt-3 w-75 bg-light mx-auto border rounded p-4">
+
     </div>
 </div>
 <?php $this->end(); ?>
