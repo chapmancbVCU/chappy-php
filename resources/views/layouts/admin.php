074fc3c40e20618130c7c5d266cfa72bdaa3d6a2
Chad Chapman <chad.chapman2010@gmail.com>
2024-12-15 11:23:22 -0500

    Added logice for vite and fixed favicon path

--- resources/views/layouts/admin.php
+++ resources/views/layouts/admin.php
@@ -10,15 +10,22 @@ use Core\FormHelper;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title><?=$this->siteTitle()?></title>
-    <link rel="icon" href="<?=APP_DOMAIN?>public/images/favicon/noun-mvc-5340614.png">
+    <link rel="icon" href="<?=APP_DOMAIN?>public/noun-mvc-5340614.png">
+    <?php if ($_ENV['APP_ENV'] === 'local'): ?>
+        <script type="module" src="http://localhost:5173/@vite/client"></script>
+        <script type="module" src="<?= vite('resources/js/app.js') ?>"></script>
+    <?php else: ?>
+        <!-- Production: Include compiled assets -->
+        <link rel="stylesheet" href="<?= vite('resources/css/app.css') ?>">
+        <script type="module" src="<?= vite('resources/js/app.js') ?>"></script>
+    <?php endif; ?>
     <link rel="stylesheet" href="<?=APP_DOMAIN?>resources/css/bootstrap-4.6.2/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="<?=APP_DOMAIN?>resources/css/styles.css?v=<?=VERSION?>" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="<?=APP_DOMAIN?>resources/css/alerts/alertMsg.min.css?v=<?=VERSION?>" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="<?=APP_DOMAIN?>resources/css/font-awesome-4.7.0/font-awesome.min.css" media="screen" title="no title" charset="utf-8">
-    <script src="<?=APP_DOMAIN?>public/js/jQuery-3.7.1/jQuery-3.7.1.min.js"></script>
-    <script src="<?=APP_DOMAIN?>public/js/alerts/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="<?=APP_DOMAIN?>public/js/bootstrap-4.6.2/bootstrap.min.js"></script>
-    <script src="<?=APP_DOMAIN?>public/js/alerts/alertMsg.min.js?v=<?=VERSION?>"></script>
+    <script src="<?=APP_DOMAIN?>resources/js/jQuery-3.7.1/jQuery-3.7.1.min.js"></script>
+    <script src="<?=APP_DOMAIN?>resources/js/alerts/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="<?=APP_DOMAIN?>resources/js/bootstrap-4.6.2/bootstrap.min.js"></script>
+    <script src="<?=APP_DOMAIN?>resources/js/alerts/alertMsg.min.js?v=<?=VERSION?>"></script>
     <?= $this->content('head'); ?>
 
   </head>
