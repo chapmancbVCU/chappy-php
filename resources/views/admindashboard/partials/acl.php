bb9a07b42b5e872c339295a889d12333b6e34a6c
Chad Chapman <chad.chapman2010@gmail.com>
2025-01-26 18:11:39 -0500

    #19 - Resolved issue where cancel and update buttons were touching text field for ACL

--- resources/views/admindashboard/partials/acl.php
+++ resources/views/admindashboard/partials/acl.php
@@ -2,7 +2,7 @@
 <form class="form" action=<?=$this->postAction?> method="post">
     <?= FormHelper::csrfInput() ?>
     <?= FormHelper::inputBlock('text', "ACL", 'acl', $this->acl->acl, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
-    <div class="col-md-12 text-end">
+    <div class="col-md-12 text-end pt-3">
         <a href="<?=APP_DOMAIN?>admindashboard/manageACLs" class="btn btn-default">Cancel</a>
         <?= FormHelper::submitTag('Update',['class'=>'btn btn-primary']) ?>
     </div>
