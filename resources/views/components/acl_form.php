7a5b860ca2bc2ecd0c937456908fb6de32da902f
Chad Chapman <chad.chapman2010@gmail.com>
2025-01-26 18:20:01 -0500

    #20 - Renamed file from acl.php to acl_form.php and moved it from partials to components.  Updated edit and add acl views

---
+++ resources/views/components/acl_form.php
@@ -0,0 +1,9 @@
+<?php use Core\FormHelper; ?>
+<form class="form" action=<?=$this->postAction?> method="post">
+    <?= FormHelper::csrfInput() ?>
+    <?= FormHelper::inputBlock('text', "ACL", 'acl', $this->acl->acl, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
+    <div class="col-md-12 text-end pt-3">
+        <a href="<?=APP_DOMAIN?>admindashboard/manageACLs" class="btn btn-default">Cancel</a>
+        <?= FormHelper::submitTag('Update',['class'=>'btn btn-primary']) ?>
+    </div>
+</form>
