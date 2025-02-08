21ee0ab89f5c5e8a0bc0beb4892f1f02d1793327
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-08 16:12:32 -0500

    Initial commit

---
+++ resources/views/api-docs/doctum.js
@@ -0,0 +1,319 @@
+var Doctum = {
+    treeJson: {"tree":{"l":0,"n":"","p":"","c":[{"l":1,"n":"[Global Namespace]","p":"[Global_Namespace]"},{"l":1,"n":"App","p":"App","c":[{"l":2,"n":"Controllers","p":"App/Controllers","c":[{"l":3,"n":"AdmindashboardController","p":"App/Controllers/AdmindashboardController"},{"l":3,"n":"ContactsController","p":"App/Controllers/ContactsController"},{"l":3,"n":"DocumentationController","p":"App/Controllers/DocumentationController"},{"l":3,"n":"HomeController","p":"App/Controllers/HomeController"},{"l":3,"n":"ProfileController","p":"App/Controllers/ProfileController"},{"l":3,"n":"RegisterController","p":"App/Controllers/RegisterController"},{"l":3,"n":"RestrictedController","p":"App/Controllers/RestrictedController"},{"l":3,"n":"UserguideController","p":"App/Controllers/UserguideController"}]},{"l":2,"n":"Models","p":"App/Models","c":[{"l":3,"n":"ACL","p":"App/Models/ACL"},{"l":3,"n":"Contacts","p":"App/Models/Contacts"},{"l":3,"n":"Login","p":"App/Models/Login"},{"l":3,"n":"ProfileImages","p":"App/Models/ProfileImages"},{"l":3,"n":"UserSessions","p":"App/Models/UserSessions"},{"l":3,"n":"Users","p":"App/Models/Users"}]}]},{"l":1,"n":"Console","p":"Console","c":[{"l":2,"n":"App","p":"Console/App","c":[{"l":3,"n":"Commands","p":"Console/App/Commands","c":[{"l":4,"n":"DropTablesCommand","p":"Console/App/Commands/DropTablesCommand"},{"l":4,"n":"GenerateControllerCommand","p":"Console/App/Commands/GenerateControllerCommand"},{"l":4,"n":"GenerateMigrationCommand","p":"Console/App/Commands/GenerateMigrationCommand"},{"l":4,"n":"GenerateModelCommand","p":"Console/App/Commands/GenerateModelCommand"},{"l":4,"n":"MakeCommand","p":"Console/App/Commands/MakeCommand"},{"l":4,"n":"MakeDBCommand","p":"Console/App/Commands/MakeDBCommand"},{"l":4,"n":"MakeDBUserCommand","p":"Console/App/Commands/MakeDBUserCommand"},{"l":4,"n":"MakeEnvCommand","p":"Console/App/Commands/MakeEnvCommand"},{"l":4,"n":"MakeProfileImagesDirCommand","p":"Console/App/Commands/MakeProfileImagesDirCommand"},{"l":4,"n":"MakeTestCommand","p":"Console/App/Commands/MakeTestCommand"},{"l":4,"n":"MigrateRefreshCommand","p":"Console/App/Commands/MigrateRefreshCommand"},{"l":4,"n":"RemoveProfileImagesCommand","p":"Console/App/Commands/RemoveProfileImagesCommand"},{"l":4,"n":"RunMigrationCommand","p":"Console/App/Commands/RunMigrationCommand"},{"l":4,"n":"RunTestCommand","p":"Console/App/Commands/RunTestCommand"}]},{"l":3,"n":"Helpers","p":"Console/App/Helpers","c":[{"l":4,"n":"Controller","p":"Console/App/Helpers/Controller"},{"l":4,"n":"Migrate","p":"Console/App/Helpers/Migrate"},{"l":4,"n":"Model","p":"Console/App/Helpers/Model"},{"l":4,"n":"ProfileImageDir","p":"Console/App/Helpers/ProfileImageDir"},{"l":4,"n":"Test","p":"Console/App/Helpers/Test"},{"l":4,"n":"Tools","p":"Console/App/Helpers/Tools"}]}]}]},{"l":1,"n":"Core","p":"Core","c":[{"l":2,"n":"Lib","p":"Core/Lib","c":[{"l":3,"n":"Utilities","p":"Core/Lib/Utilities","c":[{"l":4,"n":"UploadProfileImage","p":"Core/Lib/Utilities/UploadProfileImage"},{"l":4,"n":"Uploads","p":"Core/Lib/Utilities/Uploads"}]},{"l":3,"n":"Logger","p":"Core/Lib/Logger"}]},{"l":2,"n":"Validators","p":"Core/Validators","c":[{"l":3,"n":"CustomValidator","p":"Core/Validators/CustomValidator"},{"l":3,"n":"EmailValidator","p":"Core/Validators/EmailValidator"},{"l":3,"n":"LowerCharValidator","p":"Core/Validators/LowerCharValidator"},{"l":3,"n":"MatchesValidator","p":"Core/Validators/MatchesValidator"},{"l":3,"n":"MaxValidator","p":"Core/Validators/MaxValidator"},{"l":3,"n":"MinValidator","p":"Core/Validators/MinValidator"},{"l":3,"n":"NumberCharValidator","p":"Core/Validators/NumberCharValidator"},{"l":3,"n":"NumericValidator","p":"Core/Validators/NumericValidator"},{"l":3,"n":"RequiredValidator","p":"Core/Validators/RequiredValidator"},{"l":3,"n":"SpecialCharValidator","p":"Core/Validators/SpecialCharValidator"},{"l":3,"n":"UniqueValidator","p":"Core/Validators/UniqueValidator"},{"l":3,"n":"UpperCharValidator","p":"Core/Validators/UpperCharValidator"}]},{"l":2,"n":"Application","p":"Core/Application"},{"l":2,"n":"Controller","p":"Core/Controller"},{"l":2,"n":"Cookie","p":"Core/Cookie"},{"l":2,"n":"DB","p":"Core/DB"},{"l":2,"n":"FormHelper","p":"Core/FormHelper"},{"l":2,"n":"Helper","p":"Core/Helper"},{"l":2,"n":"Input","p":"Core/Input"},{"l":2,"n":"Migration","p":"Core/Migration"},{"l":2,"n":"Model","p":"Core/Model"},{"l":2,"n":"Router","p":"Core/Router"},{"l":2,"n":"Session","p":"Core/Session"},{"l":2,"n":"View","p":"Core/View"}]},{"l":1,"n":"Database","p":"Database","c":[{"l":2,"n":"Migrations","p":"Database/Migrations","c":[{"l":3,"n":"Migration1722819683","p":"Database/Migrations/Migration1722819683"},{"l":3,"n":"Migration1722819717","p":"Database/Migrations/Migration1722819717"},{"l":3,"n":"Migration1723159214","p":"Database/Migrations/Migration1723159214"},{"l":3,"n":"Migration1723167263","p":"Database/Migrations/Migration1723167263"},{"l":3,"n":"Migration1724274442","p":"Database/Migrations/Migration1724274442"},{"l":3,"n":"Migration1731861283","p":"Database/Migrations/Migration1731861283"}]}]},{"l":1,"n":"Tests","p":"Tests","c":[{"l":2,"n":"Test","p":"Tests/Test"}]}]},"treeOpenLevel":2},
+    /** @var boolean */
+    treeLoaded: false,
+    /** @var boolean */
+    listenersRegistered: false,
+    autoCompleteData: null,
+    /** @var boolean */
+    autoCompleteLoading: false,
+    /** @var boolean */
+    autoCompleteLoaded: false,
+    /** @var string|null */
+    rootPath: null,
+    /** @var string|null */
+    autoCompleteDataUrl: null,
+    /** @var HTMLElement|null */
+    doctumSearchAutoComplete: null,
+    /** @var HTMLElement|null */
+    doctumSearchAutoCompleteProgressBarContainer: null,
+    /** @var HTMLElement|null */
+    doctumSearchAutoCompleteProgressBar: null,
+    /** @var number */
+    doctumSearchAutoCompleteProgressBarPercent: 0,
+    /** @var autoComplete|null */
+    autoCompleteJS: null,
+    querySearchSecurityRegex: /([^0-9a-zA-Z:\\\\_\s])/gi,
+    buildTreeNode: function (treeNode, htmlNode, treeOpenLevel) {
+        var ulNode = document.createElement('ul');
+        for (var childKey in treeNode.c) {
+            var child = treeNode.c[childKey];
+            var liClass = document.createElement('li');
+            var hasChildren = child.hasOwnProperty('c');
+            var nodeSpecialName = (hasChildren ? 'namespace:' : 'class:') + child.p.replace(/\//g, '_');
+            liClass.setAttribute('data-name', nodeSpecialName);
+
+            // Create the node that will have the text
+            var divHd = document.createElement('div');
+            var levelCss = child.l - 1;
+            divHd.className = hasChildren ? 'hd' : 'hd leaf';
+            divHd.style.paddingLeft = (hasChildren ? (levelCss * 18) : (8 + (levelCss * 18))) + 'px';
+            if (hasChildren) {
+                if (child.l <= treeOpenLevel) {
+                    liClass.className = 'opened';
+                }
+                var spanIcon = document.createElement('span');
+                spanIcon.className = 'icon icon-play';
+                divHd.appendChild(spanIcon);
+            }
+            var aLink = document.createElement('a');
+
+            // Edit the HTML link to work correctly based on the current depth
+            aLink.href = Doctum.rootPath + child.p + '.html';
+            aLink.innerText = child.n;
+            divHd.appendChild(aLink);
+            liClass.appendChild(divHd);
+
+            // It has children
+            if (hasChildren) {
+                var divBd = document.createElement('div');
+                divBd.className = 'bd';
+                Doctum.buildTreeNode(child, divBd, treeOpenLevel);
+                liClass.appendChild(divBd);
+            }
+            ulNode.appendChild(liClass);
+        }
+        htmlNode.appendChild(ulNode);
+    },
+    initListeners: function () {
+        if (Doctum.listenersRegistered) {
+            // Quick exit, already registered
+            return;
+        }
+                Doctum.listenersRegistered = true;
+    },
+    loadTree: function () {
+        if (Doctum.treeLoaded) {
+            // Quick exit, already registered
+            return;
+        }
+        Doctum.rootPath = document.body.getAttribute('data-root-path');
+        Doctum.buildTreeNode(Doctum.treeJson.tree, document.getElementById('api-tree'), Doctum.treeJson.treeOpenLevel);
+
+        // Toggle left-nav divs on click
+        $('#api-tree .hd span').on('click', function () {
+            $(this).parent().parent().toggleClass('opened');
+        });
+
+        // Expand the parent namespaces of the current page.
+        var expected = $('body').attr('data-name');
+
+        if (expected) {
+            // Open the currently selected node and its parents.
+            var container = $('#api-tree');
+            var node = $('#api-tree li[data-name="' + expected + '"]');
+            // Node might not be found when simulating namespaces
+            if (node.length > 0) {
+                node.addClass('active').addClass('opened');
+                node.parents('li').addClass('opened');
+                var scrollPos = node.offset().top - container.offset().top + container.scrollTop();
+                // Position the item nearer to the top of the screen.
+                scrollPos -= 200;
+                container.scrollTop(scrollPos);
+            }
+        }
+        Doctum.treeLoaded = true;
+    },
+    pagePartiallyLoaded: function (event) {
+        Doctum.initListeners();
+        Doctum.loadTree();
+        Doctum.loadAutoComplete();
+    },
+    pageFullyLoaded: function (event) {
+        // it may not have received DOMContentLoaded event
+        Doctum.initListeners();
+        Doctum.loadTree();
+        Doctum.loadAutoComplete();
+        // Fire the event in the search page too
+        if (typeof DoctumSearch === 'object') {
+            DoctumSearch.pageFullyLoaded();
+        }
+    },
+    loadAutoComplete: function () {
+        if (Doctum.autoCompleteLoaded) {
+            // Quick exit, already loaded
+            return;
+        }
+        Doctum.autoCompleteDataUrl = document.body.getAttribute('data-search-index-url');
+        Doctum.doctumSearchAutoComplete = document.getElementById('doctum-search-auto-complete');
+        Doctum.doctumSearchAutoCompleteProgressBarContainer = document.getElementById('search-progress-bar-container');
+        Doctum.doctumSearchAutoCompleteProgressBar = document.getElementById('search-progress-bar');
+        if (Doctum.doctumSearchAutoComplete !== null) {
+            // Wait for it to be loaded
+            Doctum.doctumSearchAutoComplete.addEventListener('init', function (_) {
+                Doctum.autoCompleteLoaded = true;
+                Doctum.doctumSearchAutoComplete.addEventListener('selection', function (event) {
+                    // Go to selection page
+                    window.location = Doctum.rootPath + event.detail.selection.value.p;
+                });
+                Doctum.doctumSearchAutoComplete.addEventListener('navigate', function (event) {
+                    // Set selection in text box
+                    if (typeof event.detail.selection.value === 'object') {
+                        Doctum.doctumSearchAutoComplete.value = event.detail.selection.value.n;
+                    }
+                });
+                Doctum.doctumSearchAutoComplete.addEventListener('results', function (event) {
+                    Doctum.markProgressFinished();
+                });
+            });
+        }
+        // Check if the lib is loaded
+        if (typeof autoComplete === 'function') {
+            Doctum.bootAutoComplete();
+        }
+    },
+    markInProgress: function () {
+            Doctum.doctumSearchAutoCompleteProgressBarContainer.className = 'search-bar';
+            Doctum.doctumSearchAutoCompleteProgressBar.className = 'progress-bar indeterminate';
+            if (typeof DoctumSearch === 'object' && DoctumSearch.pageFullyLoaded) {
+                DoctumSearch.doctumSearchPageAutoCompleteProgressBarContainer.className = 'search-bar';
+                DoctumSearch.doctumSearchPageAutoCompleteProgressBar.className = 'progress-bar indeterminate';
+            }
+    },
+    markProgressFinished: function () {
+        Doctum.doctumSearchAutoCompleteProgressBarContainer.className = 'search-bar hidden';
+        Doctum.doctumSearchAutoCompleteProgressBar.className = 'progress-bar';
+        if (typeof DoctumSearch === 'object' && DoctumSearch.pageFullyLoaded) {
+            DoctumSearch.doctumSearchPageAutoCompleteProgressBarContainer.className = 'search-bar hidden';
+            DoctumSearch.doctumSearchPageAutoCompleteProgressBar.className = 'progress-bar';
+        }
+    },
+    makeProgess: function () {
+        Doctum.makeProgressOnProgressBar(
+            Doctum.doctumSearchAutoCompleteProgressBarPercent,
+            Doctum.doctumSearchAutoCompleteProgressBar
+        );
+        if (typeof DoctumSearch === 'object' && DoctumSearch.pageFullyLoaded) {
+            Doctum.makeProgressOnProgressBar(
+                Doctum.doctumSearchAutoCompleteProgressBarPercent,
+                DoctumSearch.doctumSearchPageAutoCompleteProgressBar
+            );
+        }
+    },
+    loadAutoCompleteData: function (query) {
+        return new Promise(function (resolve, reject) {
+            if (Doctum.autoCompleteData !== null) {
+                resolve(Doctum.autoCompleteData);
+                return;
+            }
+            Doctum.markInProgress();
+            function reqListener() {
+                Doctum.autoCompleteLoading = false;
+                Doctum.autoCompleteData = JSON.parse(this.responseText).items;
+                Doctum.markProgressFinished();
+
+                setTimeout(function () {
+                    resolve(Doctum.autoCompleteData);
+                }, 50);// Let the UI render once before sending the results for processing. This gives time to the progress bar to hide
+            }
+            function reqError(err) {
+                Doctum.autoCompleteLoading = false;
+                Doctum.autoCompleteData = null;
+                console.error(err);
+                reject(err);
+            }
+
+            var oReq = new XMLHttpRequest();
+            oReq.onload = reqListener;
+            oReq.onerror = reqError;
+            oReq.onprogress = function (pe) {
+                if (pe.lengthComputable) {
+                    Doctum.doctumSearchAutoCompleteProgressBarPercent = parseInt(pe.loaded / pe.total * 100, 10);
+                    Doctum.makeProgess();
+                }
+            };
+            oReq.onloadend = function (_) {
+                Doctum.markProgressFinished();
+            };
+            oReq.open('get', Doctum.autoCompleteDataUrl, true);
+            oReq.send();
+        });
+    },
+    /**
+     * Make some progress on a progress bar
+     *
+     * @param number percentage
+     * @param HTMLElement progressBar
+     * @return void
+     */
+    makeProgressOnProgressBar: function(percentage, progressBar) {
+        progressBar.className = 'progress-bar';
+        progressBar.style.width = percentage + '%';
+        progressBar.setAttribute(
+            'aria-valuenow', percentage
+        );
+    },
+    searchEngine: function (query, record) {
+        if (typeof query !== 'string') {
+            return '';
+        }
+        // replace all (mode = g) spaces and non breaking spaces (\s) by pipes
+        // g = global mode to mark also the second word searched
+        // i = case insensitive
+        // how this function works:
+        // First: search if the query has the keywords in sequence
+        // Second: replace the keywords by a mark and leave all the text in between non marked
+        
+        if (record.match(new RegExp('(' + query.replace(/\s/g, ').*(') + ')', 'gi')) === null) {
+            return '';// Does not match
+        }
+
+        var replacedRecord = record.replace(new RegExp('(' + query.replace(/\s/g, '|') + ')', 'gi'), function (group) {
+            return '<mark class="auto-complete-highlight">' + group + '</mark>';
+        });
+
+        if (replacedRecord !== record) {
+            return replacedRecord;// This should not happen but just in case there was no match done
+        }
+
+        return '';
+    },
+    /**
+     * Clean the search query
+     *
+     * @param string|null query
+     * @return string
+     */
+    cleanSearchQuery: function (query) {
+        if (typeof query !== 'string') {
+            return '';
+        }
+        // replace any chars that could lead to injecting code in our regex
+        // remove start or end spaces
+        // replace backslashes by an escaped version, use case in search: \myRootFunction
+        return query.replace(Doctum.querySearchSecurityRegex, '').trim().replace(/\\/g, '\\\\');
+    },
+    bootAutoComplete: function () {
+        Doctum.autoCompleteJS = new autoComplete(
+            {
+                selector: '#doctum-search-auto-complete',
+                searchEngine: function (query, record) {
+                    return Doctum.searchEngine(query, record);
+                },
+                submit: true,
+                data: {
+                    src: function (q) {
+                        Doctum.markInProgress();
+                        return Doctum.loadAutoCompleteData(q);
+                    },
+                    keys: ['n'],// Data 'Object' key to be searched
+                    cache: false, // Is not compatible with async fetch of data
+                },
+                query: (input) => {
+                    return Doctum.cleanSearchQuery(input);
+                },
+                trigger: (query) => {
+                    return Doctum.cleanSearchQuery(query).length > 0;
+                },
+                resultsList: {
+                    tag: 'ul',
+                    class: 'auto-complete-dropdown-menu',
+                    destination: '#auto-complete-results',
+                    position: 'afterbegin',
+                    maxResults: 500,
+                    noResults: false,
+                },
+                resultItem: {
+                    tag: 'li',
+                    class: 'auto-complete-result',
+                    highlight: 'auto-complete-highlight',
+                    selected: 'auto-complete-selected'
+                },
+            }
+        );
+    }
+};
+
+
+document.addEventListener('DOMContentLoaded', Doctum.pagePartiallyLoaded, false);
+window.addEventListener('load', Doctum.pageFullyLoaded, false);
