bfceba47f9b2aa5e455ccc3ec4688958f8f708f7
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-16 10:18:04 -0500

    #27 - Added logic to turn off loggin when DEBUG env variable is set to false to save disk space in production

--- core/lib/Logger.php
+++ core/lib/Logger.php
@@ -16,36 +16,38 @@ class Logger {
      * @return void
      */
     public static function log(string $message, string $level = 'info'): void {
-        $date = date('Y-m-d H:i:s');
-        $logMessage = "[$date] [$level] $message" . PHP_EOL;
-        $logDir = dirname(self::$logFile);
-
-        // Debug: Check directory existence
-        if (!is_dir($logDir)) {
-            mkdir($logDir, 0755, true);
-        }
-
-        // Debug: Check directory permissions
-        if (!is_writable($logDir)) {
-            die("Error: Log directory is not writable. Current permissions: " . substr(sprintf('%o', fileperms($logDir)), -4));
-        }
-
-        // Debug: Check file existence
-        if (!file_exists(self::$logFile)) {
-            touch(self::$logFile);
-            chmod(self::$logFile, 0644);
-        }
-
-        // Debug: Check if file is writable
-        if (!is_writable(self::$logFile)) {
-            die("Error: Log file is not writable.");
-        }
-
-        // Write to log file
-        $result = file_put_contents(self::$logFile, $logMessage, FILE_APPEND | LOCK_EX);
-
-        if ($result === false) {
-            die("Error: Unable to write to log file.");
+        if(DEBUG == "true") {
+            $date = date('Y-m-d H:i:s');
+            $logMessage = "[$date] [$level] $message" . PHP_EOL;
+            $logDir = dirname(self::$logFile);
+    
+            // Debug: Check directory existence
+            if (!is_dir($logDir)) {
+                mkdir($logDir, 0755, true);
+            }
+    
+            // Debug: Check directory permissions
+            if (!is_writable($logDir)) {
+                die("Error: Log directory is not writable. Current permissions: " . substr(sprintf('%o', fileperms($logDir)), -4));
+            }
+    
+            // Debug: Check file existence
+            if (!file_exists(self::$logFile)) {
+                touch(self::$logFile);
+                chmod(self::$logFile, 0644);
+            }
+    
+            // Debug: Check if file is writable
+            if (!is_writable(self::$logFile)) {
+                die("Error: Log file is not writable.");
+            }
+    
+            // Write to log file
+            $result = file_put_contents(self::$logFile, $logMessage, FILE_APPEND | LOCK_EX);
+    
+            if ($result === false) {
+                die("Error: Unable to write to log file.");
+            }
         }
     }
 }
