f521b92677a6941af896727649366c04d67b0e60
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-15 19:38:44 -0500

    Resoled issues if permissions is not correct for logging

--- core/lib/Logger.php
+++ core/lib/Logger.php
@@ -18,16 +18,33 @@ class Logger {
     public static function log(string $message, string $level = 'info'): void {
         $date = date('Y-m-d H:i:s');
         $logMessage = "[$date] [$level] $message" . PHP_EOL;
+        $logDir = dirname(self::$logFile);
 
-        // Debugging: Check if file is writable
-        if(!is_writable(dirname(self::$logFile))) {
-            die("Error: Log directory is not writable.");
+        // Debug: Check directory existence
+        if (!is_dir($logDir)) {
+            mkdir($logDir, 0755, true);
         }
 
-        // Debugging: Try writing manually
+        // Debug: Check directory permissions
+        if (!is_writable($logDir)) {
+            die("Error: Log directory is not writable. Current permissions: " . substr(sprintf('%o', fileperms($logDir)), -4));
+        }
+
+        // Debug: Check file existence
+        if (!file_exists(self::$logFile)) {
+            touch(self::$logFile);
+            chmod(self::$logFile, 0644);
+        }
+
+        // Debug: Check if file is writable
+        if (!is_writable(self::$logFile)) {
+            die("Error: Log file is not writable.");
+        }
+
+        // Write to log file
         $result = file_put_contents(self::$logFile, $logMessage, FILE_APPEND | LOCK_EX);
 
-        if($result === false) {
+        if ($result === false) {
             die("Error: Unable to write to log file.");
         }
     }
