9b62425b645f158c996234e1e7f9e6930fb18507
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-17 20:55:14 -0500

    #30 - Added console command to run docs

---
+++ src/console/commands/ServeUserGuideCommand.php
@@ -0,0 +1,59 @@
+<?php
+namespace Console\App\Commands;
+ 
+use Console\App\Helpers\Tools;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Performs the command for serving user guide locally: 
+ * 
+ * bundle exec jekyll serve
+ */
+class ServeUserGuideCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('serve:docs')
+            ->setDescription('Serves user guide locally')
+            ->setHelp('run serve:docs to serve user guide locally');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return integer
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Change to the `docs` directory and serve the Jekyll site
+        $command = 'cd docs && bundle exec jekyll serve';
+        
+        // Execute command and capture output
+        $process = popen($command, 'r');
+        
+        if (!$process) {
+            $output->writeln('<error>Failed to start Jekyll server</error>');
+            return Command::FAILURE;
+        }
+
+        // Stream output to console
+        while (!feof($process)) {
+            $line = fgets($process);
+            if ($line !== false) {
+                $output->writeln(trim($line));
+            }
+        }
+        
+        pclose($process);
+
+        return Command::SUCCESS;
+    }
+}
