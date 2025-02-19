64cc4eb0a89f7da1f4af5644bf40128795dda1b0
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-19 18:11:59 -0500

    #32 - Implemented console command for removing logs

---
+++ src/console/commands/RemoveLogsCommand.php
@@ -0,0 +1,44 @@
+<?php
+namespace Console\App\Commands;
+ 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Console\App\Helpers\Tools;
+
+/**
+ * Supports ability to delete log file.
+ */
+class RemoveLogsCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('log:clear')
+            ->setDescription('Removes log file')
+            ->setHelp('Run php console log:clear to remove log files.');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return integer
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // When successful
+        if(unlink(ROOT.DS.'storage'.DS.'logs'.DS.'app.log')) {
+            Tools::info('Log file successfully cleared', 'green');
+            return COMMAND::SUCCESS;
+        } 
+
+        // Always execute when there is a failure.
+        Tools::info('There was an issue removing the log file', 'red');
+        return COMMAND::FAILURE;
+    }
+}
