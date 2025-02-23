8c808bc60d5a8f76ecf85645a21e2d7e7ee22cfe
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-23 18:10:36 -0500

    #10 - Implemented command for creating new database seeder

---
+++ src/console/commands/MakeSeederCommand.php
@@ -0,0 +1,38 @@
+<?php
+namespace Console\App\Commands;
+ 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Console\App\Helpers\DBSeeder;
+
+/**
+ * Undocumented class
+ */
+class MakeSeederCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('make:seeder')
+            ->setDescription('Generates a new Seeder class')
+            ->setHelp('php console make:seeder ClassName')
+            ->addArgument('seeder-name', InputArgument::REQUIRED, 'Pass the name of the seeder class you want to create');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return integer
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return DBSeeder::makeSeeder($input);
+    }
+}
--- src/console/helpers/DBSeeder.php
+++ src/console/helpers/DBSeeder.php
@@ -3,11 +3,67 @@ namespace Console\App\Helpers;
 
 use Database\Seeders\DatabaseSeeder;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 /**
  * Supports operations related to database seeding.
  */
 class DBSeeder {
 
+    public static function makeSeeder(InputInterface $input): int {
+        $seederName = $input->getArgument('seeder-name');
+        if (php_sapi_name() != 'cli') die('Restricted');
+        $ext = ".php";
+        $fullPath = ROOT.DS.'database'.DS.'seeders'.DS.$seederName.'TableSeeder'.$ext;
+        $content = '<?php
+namespace Database\Seeders;
+
+use Faker\Factory as Faker;
+use Core\Lib\Database\Seeder;
+use Console\App\Helpers\Tools;
+
+// Import your model
+use App\Models\\'.ucfirst($seederName).';
+
+/**
+ * Seeder for '.lcfirst($seederName).' table.
+ * 
+ * @return void
+ */
+class '.ucfirst($seederName).'TableSeeder extends Seeder {
+    /**
+     * Runs the database seeder
+     *
+     * @return void
+     */
+    public function run(): void {
+        $faker = Faker::create();
+        
+        // Set number of records to create.
+        $numberOfRecords = 10;
+        $i = 0;
+        while($i < $numberOfRecords) {
+            $'.lcfirst($seederName).' = new '.ucfirst($seederName).'();
+            
+
+            if($'.lcfirst($seederName).'->save()) {
+                $i++;
+            }
+        }
+        Tools::info("Seeded '.lcfirst($seederName).' table.");
+    }
+}
+';
+        if(!file_exists($fullPath)) {
+            $resp = file_put_contents($fullPath, $content);
+        } else {
+            Tools::info('Seeder class already exists', 'red');
+            return Command::FAILURE;
+        }
+
+        Tools::info('Seeder class successfully created');
+        return Command::SUCCESS;
+    }
+    
     /**
      * Runs command for seeding database.
      *
