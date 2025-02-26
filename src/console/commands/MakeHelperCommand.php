55ae4837def38e310263410d4f7010785c965421
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-25 19:57:32 -0500

    Initial commit

---
+++ src/console/commands/MakeHelperCommand.php
@@ -0,0 +1,35 @@
+<?php
+namespace Console\Commands;
+ 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Undocumented class
+ */
+class MakeHelperCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        //
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return integer
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        //
+    }
+}
