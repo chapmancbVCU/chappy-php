873d9c9a558e8cfbe5c49da765ebdac59028b29e
Chad Chapman <chad.chapman2010@gmail.com>
2025-02-25 19:58:30 -0500

    Moved implementation for making command to its own helper file

--- src/console/commands/MakeCommand.php
+++ src/console/commands/MakeCommand.php
@@ -1,7 +1,7 @@
 <?php
 namespace Console\Commands;
 
-use Console\Helpers\Tools;
+use Console\Helpers\CommandHelper;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -30,58 +30,10 @@ class MakeCommand extends Command
      *
      * @param InputInterface $input The input.
      * @param OutputInterface $output The output.
-     * @return integer
+     * @return int Stat
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $commandName = $input->getArgument('command-name');
-        if (php_sapi_name() != 'cli') die('Restricted');
-        $ext = ".php";
-        $fullPath = ROOT.DS.'src'.DS.'console'.DS.'commands'.DS.$commandName.'Command'.$ext;
-        $content = '<?php
-namespace Console\Commands;
- 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-
-/**
- * Undocumented class
- */
-class '.$commandName.'Command extends Command {
-    /**
-     * Configures the command.
-     *
-     * @return void
-     */
-    protected function configure(): void
-    {
-        //
-    }
-
-    /**
-     * Executes the command
-     *
-     * @param InputInterface $input The input.
-     * @param OutputInterface $output The output.
-     * @return integer
-     */
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        //
-    }
-}
-';
-        if(!file_exists($fullPath)) {
-            $resp = file_put_contents($fullPath, $content);
-        } else {
-            Tools::info('Command already exists', 'red');
-            return Command::FAILURE;
-        }
-
-        Tools::info('Command successfully created');
-        return Command::SUCCESS;
+       return CommandHelper::makeCommand($input);
     }
 }
