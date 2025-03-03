544cabab6fdd0b70881c3ba991afa7338ebb84ef
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-02 20:20:10 -0500

    #27 - Added support for cli logging and updated api-docs

--- src/Console/Helpers/Tools.php
+++ src/Console/Helpers/Tools.php
@@ -1,6 +1,7 @@
 <?php
 namespace Console\Helpers;
 
+use Core\Lib\Logger;
 use Symfony\Component\Console\Command\Command;
 
 /**
@@ -20,6 +21,8 @@ class Tools {
      * Generates output messages for console commands.
      *
      * @param string $message The message we want to show.
+     * @param string $level The level of severity for log file.  The valid 
+     * levels are info, debug, warning, error, critical, alert, and emergency.
      * @param string $background The background color.  This function 
      * supports black, red, green, yellow, blue, magenta, cyan, and 
      * light-grey
@@ -29,37 +32,27 @@ class Tools {
      * light-magenta.
      * @return void
      */
-    public static function info(string $message, string $background = BACKGROUND_COLOR, $text = TEXT_COLOR): void {
+    public static function info(string $message, string $level = 'info', string $background = BACKGROUND_COLOR, $text = TEXT_COLOR): void {
         $backgroundColor = [
-            'black' => '40',
-            'red' => '41',
-            'green' => '42',
-            'yellow' => '43',
-            'blue' => '44',
-            'magenta' => '45',
-            'cyan' => '46',
-            'light-grey' => '47'
+            'black' => '40', 'red' => '41', 'green' => '42', 'yellow' => '43',
+            'blue' => '44', 'magenta' => '45', 'cyan' => '46', 'light-grey' => '47'
         ];
 
         $textColor = [
-            'black' => '0;30',
-            'white' => '1;37',
-            'dark-grey' => '1;30',
-            'red' => '0;31',
-            'green' => '0;32',
-            'brown' => '0;33',
-            'yellow' => '1;33',
-            'blue' => '0;34',
-            'magenta' => '0;35',
-            'cyan' => '0;36',
-            'light-cyan' => '1;36',
-            'light-grey' => '0;37',
-            'light-red' => '1;31',
-            'light-green' => '1;32',
-            'light-blue' => '1;34',
-            'light-magenta' => '1;35'
+            'black' => '0;30', 'white' => '1;37', 'dark-grey' => '1;30', 'red' => '0;31',
+            'green' => '0;32', 'brown' => '0;33', 'yellow' => '1;33', 'blue' => '0;34',
+            'magenta' => '0;35', 'cyan' => '0;36', 'light-cyan' => '1;36', 'light-grey' => '0;37',
+            'light-red' => '1;31', 'light-green' => '1;32', 'light-blue' => '1;34', 'light-magenta' => '1;35'
         ];
         
+        // Validate severity level and log to file.
+        $validLevels = ['info', 'debug', 'warning', 'error', 'critical', 'alert', 'emergency'];
+        if (!in_array(strtolower($level), $validLevels)) {
+            $level = 'info'; // Default to 'info' if invalid level provided
+        }
+        Logger::log($message, $level);
+
+        // Perform console logging
         if(array_key_exists($background, $backgroundColor) && array_key_exists($text, $textColor)) {
             echo "\e[".$textColor[$text].";".$backgroundColor[$background]."m\n\n"."   ".$message."\n\e[0m\n";
         } else {
