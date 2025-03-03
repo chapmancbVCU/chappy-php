0ff5b5435dbcbb0250df6c01608dbdee498157f3
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-03 18:57:01 -0500

    #49 - Updated config file to support multiple database types

--- config/database.php
+++ config/database.php
@@ -1,6 +1,39 @@
 <?php
 
-define('DB_NAME', $_ENV['DB_NAME']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('DB_HOST', $_ENV['DB_HOST']);
+return [
+    'default' => $_ENV['DB_CONNECTION'] ?? 'mysql', // Default to MySQL if not specified
+
+    'connections' => [
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => $_ENV['DB_DATABASE'] ?? __DIR__ . '/../../database/database.sqlite',
+            'prefix' => '',
+        ],
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
+            'port' => $_ENV['DB_PORT'] ?? '3306',
+            'database' => $_ENV['DB_DATABASE'] ?? 'mvc',
+            'username' => $_ENV['DB_USERNAME'] ?? 'root',
+            'password' => $_ENV['DB_PASSWORD'] ?? '',  // Corrected to use $_ENV
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+        ],
+
+        'mariadb' => [
+            'driver' => 'mysql', // MariaDB is compatible with MySQL driver
+            'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
+            'port' => $_ENV['DB_PORT'] ?? '3306',
+            'database' => $_ENV['DB_DATABASE'] ?? 'mvc',
+            'username' => $_ENV['DB_USERNAME'] ?? 'root',
+            'password' => $_ENV['DB_PASSWORD'] ?? '',  // Corrected to use $_ENV
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+        ],
+    ]
+];
