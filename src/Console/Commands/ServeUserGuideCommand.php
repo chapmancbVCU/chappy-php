48c1a337fe1ebf5803ef7df66d7a625b9c26d451
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-05 18:55:07 -0500

    #4 - Updated command so you can set ports

--- src/Console/Commands/ServeUserGuideCommand.php
+++ src/Console/Commands/ServeUserGuideCommand.php
@@ -4,40 +4,42 @@ namespace Console\Commands;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
- * Performs the command for serving user guide locally: 
- * 
- * bundle exec jekyll serve
+ * Performs the command for serving the Jekyll user guide locally.
  */
 class ServeUserGuideCommand extends Command {
     /**
      * Configures the command.
-     *
-     * @return void
      */
     protected function configure(): void
     {
-        $this->setName('serve:docs')
-            ->setDescription('Serves user guide locally')
-            ->setHelp('run php console serve:docs to serve user guide locally');
+        $this
+            ->setName('serve:docs')
+            ->setDescription('Serves the user guide locally using Jekyll')
+            ->setHelp('Run php console serve:docs --host=127.0.0.1 --port=4000 to serve the user guide')
+            ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'Host address', '127.0.0.1')
+            ->addOption('port', null, InputOption::VALUE_OPTIONAL, 'Port number', 4000);
     }
 
     /**
-     * Executes the command
-     *
-     * @param InputInterface $input The input.
-     * @param OutputInterface $output The output.
-     * @return int A value that indicates success, invalid, or failure.
+     * Executes the command.
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // Change to the `docs` directory and serve the Jekyll site
-        $command = 'cd docs && bundle exec jekyll serve';
-        
+        $host = $input->getOption('host') ?: '127.0.0.1';
+        $port = (int) $input->getOption('port') ?: 4000;
+
+        // Change to the `docs` directory and serve the Jekyll site with specified host and port
+        $command = sprintf('cd docs && bundle exec jekyll serve --host=%s --port=%d', escapeshellarg($host), $port);
+
+        $output->writeln("<info>Starting Jekyll server at http://{$host}:{$port}</info>");
+        $output->writeln("<info>Press Ctrl+C to stop the server.</info>");
+
         // Execute command and capture output
         $process = popen($command, 'r');
-        
+
         if (!$process) {
             $output->writeln('<error>Failed to start Jekyll server</error>');
             return Command::FAILURE;
