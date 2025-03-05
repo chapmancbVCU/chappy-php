76988fceaf6e801a64ec15197ed2060bfe64db59
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-05 18:50:24 -0500

    #50 - Fixed typo in port argument setup

--- src/Console/Commands/ServeCommand.php
+++ src/Console/Commands/ServeCommand.php
@@ -21,7 +21,7 @@ class ServeCommand extends Command {
             ->setDescription('Starts built-in PHP server')
             ->setHelp('run php console serve and navigate to localhost:8000')
             ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'Host Address', 'localhost')
-            ->addOption('port', 'null', InputOption::VALUE_OPTIONAL, 'Port number', 8000);
+            ->addOption('port', null, InputOption::VALUE_OPTIONAL, 'Port number', 8000);
     }
 
     /**
