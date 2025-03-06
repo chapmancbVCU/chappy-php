8729f583e7093736199bf3e0958c7b9934155cdc
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-05 19:36:34 -0500

    #4 - Added phpdoc and generate api-docs

--- src/Console/Helpers/Log.php
+++ src/Console/Helpers/Log.php
@@ -7,7 +7,14 @@ use Symfony\Component\Console\Command\Command;
  * Supports ability to manage logs.
  */
 class Log {
-    public static function delete(string $message, string $path) {
+    /**
+     * Performs delete operation on log files
+     *
+     * @param string $message The message to be displayed.
+     * @param string $path The full path to the log file to be deleted.
+     * @return void
+     */
+    public static function delete(string $message, string $path): void {
         if(unlink($path)) Tools::info($message, 'green');
     }
 }
