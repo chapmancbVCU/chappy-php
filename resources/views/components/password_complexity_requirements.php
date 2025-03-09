72a167c08ee1db3401aa9219171a8dbb8d0c2cb0
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-09 14:22:13 -0400

    #51 - Updated component to use Env class

--- resources/views/components/password_complexity_requirements.php
+++ resources/views/components/password_complexity_requirements.php
@@ -1,29 +1,32 @@
+<?php use Core\Lib\Utilities\Env; ?>
+
 <div>
     <h4>Password Requirements</h4>
     <ul class="pl-3">
-        <?php if(SET_PW_MIN_LENGTH == "true"):?>
-            <li>Minimum <?= PW_MIN_LENGTH ?> characters in length</li>
+        <?php if (Env::get('SET_PW_MIN_LENGTH', false)): ?>
+            <li>Minimum <?= Env::get('PW_MIN_LENGTH', 12) ?> characters in length</li>
         <?php endif; ?>
 
-        <?php if(SET_PW_MAX_LENGTH == "true"):?>
-            <li>Maximum of <?= PW_MAX_LENGTH ?> characters in length</li>
+        <?php if (Env::get('SET_PW_MAX_LENGTH', false)): ?>
+            <li>Maximum of <?= Env::get('PW_MAX_LENGTH', 30) ?> characters in length</li>
         <?php endif; ?>
 
-        <?php if(PW_UPPER_CHAR == "true"):?>
+        <?php if (Env::get('PW_UPPER_CHAR', false)): ?>
             <li>At least 1 upper case character</li>
         <?php endif; ?>
 
-        <?php if(PW_LOWER_CHAR == "true"):?>
+        <?php if (Env::get('PW_LOWER_CHAR', false)): ?>
             <li>At least 1 lower case character</li>
         <?php endif; ?>
 
-        <?php if(PW_NUM_CHAR == "true"):?>
+        <?php if (Env::get('PW_NUM_CHAR', false)): ?>
             <li>At least 1 number</li>
         <?php endif; ?>
 
-        <?php if(PW_SPECIAL_CHAR == "true"):?>
+        <?php if (Env::get('PW_SPECIAL_CHAR', false)): ?>
             <li>Must contain at least 1 special character</li>
         <?php endif; ?>  
+
         <li>Must not contain any spaces</li>
     </ul>
 </div>
