880cf587db4fdcce9e431f7bd2b1a8bfe4697f8a
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-08 20:59:00 -0500

    #4 - Generated api-docs

--- src/scripts/helpers.php
+++ src/scripts/helpers.php
@@ -1,33 +1,35 @@
 <?php
-
-/**
- * Loads configuration files.
- *
- * @return void
- */
-function loadConfig() {
-    foreach (glob(ROOT . DS . 'config' . DS . '*.php') as $configFile) {
-        require_once $configFile;
-    }
-}
-
-/**
- * Generate the URL for a Vite asset.
- *
- * @param string $asset Path to the asset (e.g., 'resources/js/app.js').
- * @return string The URL to the asset.
- */
-function vite(string $asset): string {
-    $devServer = 'http://localhost:5173';
-    $manifestPath = __DIR__ . '/../public/build/manifest.json';
-
-    if (is_file($manifestPath)) {
-        $manifest = json_decode(file_get_contents($manifestPath), true);
-        if (isset($manifest[$asset])) {
-            return '/build/' . $manifest[$asset]['file'];
+if(!function_exists('loadConfig')) {
+    /**
+     * Loads configuration files.
+     *
+     * @return void
+     */
+    function loadConfig() {
+        foreach (glob(ROOT . DS . 'config' . DS . '*.php') as $configFile) {
+            require_once $configFile;
         }
     }
-
-    return "$devServer/$asset";
+}
+if(!function_exists('vite')){
+    /**
+     * Generate the URL for a Vite asset.
+     *
+     * @param string $asset Path to the asset (e.g., 'resources/js/app.js').
+     * @return string The URL to the asset.
+     */
+    function vite(string $asset): string {
+        $devServer = 'http://localhost:5173';
+        $manifestPath = __DIR__ . '/../public/build/manifest.json';
+    
+        if (is_file($manifestPath)) {
+            $manifest = json_decode(file_get_contents($manifestPath), true);
+            if (isset($manifest[$asset])) {
+                return '/build/' . $manifest[$asset]['file'];
+            }
+        }
+    
+        return "$devServer/$asset";
+    }
 }
 
