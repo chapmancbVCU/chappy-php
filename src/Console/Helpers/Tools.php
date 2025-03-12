d6ac554db4f62fcc2bc1502524ef9a63369ad1e6
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-11 20:54:20 -0400

    #53 - Updated to use Arr class

--- src/Console/Helpers/Tools.php
+++ src/Console/Helpers/Tools.php
@@ -38,32 +38,37 @@ class Tools {
         // Load default colors from .env if not provided
         $background = $background ?? Env::get('BACKGROUND_COLOR', 'green'); // Default: green
         $text = $text ?? Env::get('TEXT_COLOR', 'light-grey'); // Default: light-grey
+    
+        // Background color mapping
         $backgroundColor = [
             'black' => '40', 'red' => '41', 'green' => '42', 'yellow' => '43',
             'blue' => '44', 'magenta' => '45', 'cyan' => '46', 'light-grey' => '47'
         ];
-
+    
+        // Text color mapping
         $textColor = [
             'black' => '0;30', 'white' => '1;37', 'dark-grey' => '1;30', 'red' => '0;31',
             'green' => '0;32', 'brown' => '0;33', 'yellow' => '1;33', 'blue' => '0;34',
             'magenta' => '0;35', 'cyan' => '0;36', 'light-cyan' => '1;36', 'light-grey' => '0;37',
             'light-red' => '1;31', 'light-green' => '1;32', 'light-blue' => '1;34', 'light-magenta' => '1;35'
         ];
-        
-        // Validate severity level and log to file.
+    
+        // Validate severity level and log to file
         $validLevels = ['info', 'debug', 'warning', 'error', 'critical', 'alert', 'emergency'];
-        if (!array_key_exists(strtolower($level), $validLevels)) {
+        if (!in_array(strtolower($level), $validLevels, true)) {
             $level = 'info'; // Default to 'info' if invalid level provided
         }
         Logger::log($message, $level);
-
+    
         // Perform console logging
-        if(array_key_exists($background, $backgroundColor) && array_key_exists($text, $textColor)) {
-            echo "\e[".$textColor[$text].";".$backgroundColor[$background]."m\n\n"."   ".$message."\n\e[0m\n";
+        if (isset($backgroundColor[$background], $textColor[$text])) {
+            echo "\e[" . $textColor[$text] . ";" . $backgroundColor[$background] . "m\n\n   " . $message . "\n\e[0m\n";
         } else {
-            echo "\e[0;37;41m\n\n"."   Invalid background or text color.\n\e[0m\n";
+            echo "\e[0;37;41m\n\n   Invalid background or text color.\n\e[0m\n";
         }
     }
+    
+    
 
     /**
      * Generates files for console commands
