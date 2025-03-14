740b675632ba199b3df105b42a2da41f7a2a4287
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-14 19:45:25 -0400

    #54 - Undo all changes

---
+++ src/Console/Commands/UploadsCommand.php
@@ -0,0 +1,46 @@
+<?php
+namespace Console\Commands;
+
+use Console\Helpers\Tools;
+use Console\Helpers\Uploads;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+/**
+ * Supports ability to create a class that extends the Uploads class.
+ */
+class UploadsCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('make:upload')
+            ->setDescription('Generates a class that extends the Uploads class')
+            ->setHelp('php console make:upload <name_of_upload_type>')
+            ->addArgument('upload-type-name', InputArgument::REQUIRED, 'Pass the name of the type of upload you want to create.');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $uploadName = $input->getArgument('upload-type-name');
+        
+        // Generate Uploads class.
+        return Tools::writeFile(
+            ROOT.DS.'app'.DS.'lib'.DS.'utilities'.DS.'Upload'.$uploadName.'.php',
+            Uploads::makeUpload($uploadName),
+            'Upload'
+        );
+    }
+}
---
+++ src/core/Lib/FileSystem/UploadProfileImage.php
@@ -0,0 +1,59 @@
+<?php
+namespace Core\Lib\FileSystem;
+
+use Core\Helper;
+
+/**
+ * Supports the ability to upload a single profile image at a time.
+ */
+class UploadProfileImage extends Uploads {
+    /**
+     * Creates instance of UploadProfileImage class.
+     *
+     * @param array|string $files Array of files or the name to be uploaded.
+     * @param array $fileTypes An array containing a list of acceptable file 
+     * types for a particular upload action.
+     * @param int $maxAllowedSize Maximum allowable size for a particular 
+     * file.  This can vary depending on requirements.
+     * @param bool $multiple A boolean flag to set whether or not we are 
+     * working with a single file upload or an array regarding form setup.
+     * @param string $bucket The location where the files will be stored.
+     * @param string $sizeMsg The message describing the maximum allowable 
+     * size usually described as <size_as_an_int><bytes|mb|gb> (e.g.: 5mb).
+     */
+    public function __construct(array|string $files, array $fileTypes, int $maxAllowedSize, bool $multiple, string $bucket, string $sizeMsg) {
+        parent::__construct($files, $fileTypes, $maxAllowedSize, $multiple, $bucket, $sizeMsg);
+    }
+
+    /**
+     * Performs validation on profile image uploads.  This function focuses on 
+     * file size and file type.
+     *
+     * @return void
+     */
+    public function runValidation(): void {
+        $this->validateSize();
+        $this->validateFileType();
+    }
+
+    /**
+     * Validates file type and sets error message if file type is invalid.
+     *
+     * @return void
+     */
+    protected function validateFileType(): void { 
+        $reportTypes = [];
+        foreach($this->_allowedFileTypes as $type) {
+            array_push($reportTypes, image_type_to_mime_type($type));
+        }
+
+        foreach($this->_files as $file) {
+            // checking file type
+            if(!in_array(exif_imagetype($file['tmp_name']), $this->_allowedFileTypes)){
+                $name = $file['name'];
+                $msg = $name . " is not an allowed file type. Please use the following types: " . implode(', ', $reportTypes);
+                $this->addErrorMessage($name, $msg);
+            }
+        }
+    }
+}
