bc1663aece80b7d8ee4263cae131162c927bc1b8
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-14 23:10:19 -0400

    Installed correct currently maintained fontawesome package and updated layouts

--- resources/views/layouts/default.php
+++ resources/views/layouts/default.php
@@ -21,7 +21,7 @@ use Core\Lib\Utilities\Env;
     <?php endif; ?>
     <link rel="stylesheet" href="<?=Env::get('APP_DOMAIN', '/')?>node_modules/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="<?=Env::get('APP_DOMAIN', '/')?>resources/css/alerts/alertMsg.min.css?v=<?=Env::get('VERSION')?>" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="<?=Env::get('APP_DOMAIN', '/')?>node_modules/font-awesome/css/font-awesome.min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="<?=Env::get('APP_DOMAIN', '/')?>node_modules/@fortawesome/fontawesome-free/css/all.min.css" media="screen" title="no title" charset="utf-8">
     <script src="<?=Env::get('APP_DOMAIN', '/')?>node_modules/jquery/dist/jquery.min.js"></script>
     <script src="<?=Env::get('APP_DOMAIN', '/')?>node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
     <script src="<?=Env::get('APP_DOMAIN', '/')?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
